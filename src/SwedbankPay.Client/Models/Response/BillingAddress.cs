5268d4c752d03f237d9b31bfa3ffbc079e057c90
Karl Fristedt <karl.fristedt@authority.se>
2019-09-30 14:51:32 +0200

    Added models for billing and shipping details

---
+++ src/SwedbankPay.Client/Models/Response/BillingAddress.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SwedbankPay.Client.Models.Response
+{
+    public class BillingAddress
+    {
+        [JsonProperty("addressee")]
+        public string Addressee { get; set; }
+        [JsonProperty("streetAddress")]
+        public string StreetAddress { get; set; }
+        [JsonProperty("zipCode")]
+        public string ZipCode { get; set; }
+        [JsonProperty("city")]
+        public string City { get; set; }
+        [JsonProperty("countryCode")]
+        public string CountryCode { get; set; }
+    }
+}
---
+++ src/SwedbankPay.Client/Models/Response/ShippingAddress.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace SwedbankPay.Client.Models.Response
+{
+    public class ShippingAddress
+    {
+        [JsonProperty("addressee")]
+        public string Addressee { get; set; }
+        [JsonProperty("coAddress")]
+        public string CoAddress { get; set; }
+        [JsonProperty("streetAddress")]
+        public string StreetAddress { get; set; }
+        [JsonProperty("zipCode")]
+        public string ZipCode { get; set; }
+        [JsonProperty("city")]
+        public string City { get; set; }
+        [JsonProperty("countryCode")]
+        public string CountryCode { get; set; }
+    }
+}
