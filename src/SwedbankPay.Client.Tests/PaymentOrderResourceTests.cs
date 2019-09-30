4bd2e29d7c64450ce6e7c459aac28925419f6317
Karl Fristedt <karl.fristedt@authority.se>
2019-09-30 14:49:54 +0200

    Added MerchantId to SwedbankPayOptions

--- src/SwedbankPay.Client.Tests/PaymentOrderResourceTests.cs
+++ src/SwedbankPay.Client.Tests/PaymentOrderResourceTests.cs
@@ -240,7 +240,8 @@ namespace SwedbankPay.Client.Tests
                 Token = "588431aa485611f8fce876731a1734182ca0c44fcad6b8d989e22f444104aadf",
                 CallBackUrl = new Uri("https://payex.eu.ngrok.io/payment-callback?orderGroupId={orderGroupId}"),
                 CompletePageUrl = new Uri("https://payex.eu.ngrok.io/sv/checkout-sv/PayexCheckoutConfirmation/?orderGroupId={orderGroupId}"),
-                CancelPageUrl = new Uri("https://payex.eu.ngrok.io/payment-canceled?orderGroupId={orderGroupId}")
+                CancelPageUrl = new Uri("https://payex.eu.ngrok.io/payment-canceled?orderGroupId={orderGroupId}"),
+                MerchantId = "91a4c8e0-72ac-425c-a687-856706f9e9a1"
             };
 
             _sut = new SwedbankPayClient(swedbankPayOptions);
