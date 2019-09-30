653f3f1260e58844e05371dec153872168556771
Daniel Sundqvist <daniel.sundqvist@authority.se>
2019-09-30 09:28:06 +0200

    Added PaymentResource to Client

--- src/SwedbankPay.Client/SwedbankPayClient.cs
+++ src/SwedbankPay.Client/SwedbankPayClient.cs
@@ -7,6 +7,7 @@
     public class SwedbankPayClient : ISwedbankPayClient
     {
         public IPaymentOrdersResource PaymentOrders { get; }
+        public IPaymentResource PaymentResource { get; }
         public IConsumerResource Consumer { get; }
 
         public SwedbankPayClient(SwedbankPayOptions swedbankPayOptions, ILogPayExHttpResponse logger = null)
@@ -14,6 +15,7 @@
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
             var swedbankLogger = logger ?? new NoOpLogger();
             PaymentOrders = new PaymentOrdersResource(swedbankPayOptions, swedbankLogger);
+            PaymentResource new PaymentResource(swedbankPayOptions, swedbankLogger);
             Consumer = new ConsumerResource(swedbankPayOptions, swedbankLogger);
         }
     }
