d2e40e49384364b0af8f2596146f781e72d85310
Karl Fristedt <karl.fristedt@authority.se>
2019-09-30 09:07:44 +0200

    Added ConsumerResource test classes

---
+++ src/SwedbankPay.Client.Tests/TestBuilders/ConsumerResourceRequestConsumerBuilder.cs
@@ -0,0 +1,18 @@
+﻿using SwedbankPay.Client.Models.Request;
+
+namespace SwedbankPay.Client.Tests.TestBuilders
+{
+    public class ConsumerResourceRequestContainerBuilder
+    {
+        private ConsumerResourceRequestContainer _consumerResourceRequestContainer = new ConsumerResourceRequestContainer
+        {
+            ConsumerResourceRequest = new ConsumerResourceRequest()
+        };
+
+        public ConsumerResourceRequestContainerBuilder WithTestValues()
+        {
+            _consumerResourceRequestContainer.ConsumerResourceRequest.ConsumerCountryCode = "SV";
+            return this;
+        }
+    }
+}
