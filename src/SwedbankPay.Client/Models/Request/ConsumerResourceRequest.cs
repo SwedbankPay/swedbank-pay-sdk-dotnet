5d22553eb506d319af8346e164288899be4955a7
Karl Fristedt <karl.fristedt@authority.se>
2019-09-30 09:08:55 +0200

    Added ConsumerResourceRequestContainer class

---
+++ src/SwedbankPay.Client/Models/Request/ConsumerResourceRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwedbankPay.Client.Models.Request
+{
+    public class ConsumerResourceRequest
+    {
+        public string Operation { get; internal set; }
+        public string Msisdn { get; set; }
+        public string Email { get; set; }
+
+        public string ConsumerCountryCode { get; set; }
+
+        public NationalIdentifier NationalIdentifier { get; set; }
+    }
+}
