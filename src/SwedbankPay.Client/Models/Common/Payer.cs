0d3ca2584f07f389729a311b2b8b8aa9365d526c
Karl Fristedt <karl.fristedt@authority.se>
2019-10-15 13:41:58 +0200

    Added Email, Msisdn, HomePhoneNumber and WorkPhoneNumber properties

--- src/SwedbankPay.Client/Models/Common/Payer.cs
+++ src/SwedbankPay.Client/Models/Common/Payer.cs
@@ -6,5 +6,25 @@
         /// The consumer profile reference as obtained through the Consumers API.
         /// </summary>
         public string ConsumerProfileRef { get; set; }
+
+        /// <summary>
+        /// Optional (increases chance for challenge flow if not set)
+        /// </summary>
+        public string Email { get; set; }
+        
+        /// <summary>
+        /// Optional (increases chance for challenge flow if not set)
+        /// </summary>
+        public string Msisdn { get; set; }
+
+        /// <summary>
+        /// Optional (increases chance for challenge flow if not set)
+        /// </summary>
+        public string HomePhoneNumber { get; set; }
+        
+        /// <summary>
+        /// Optional (increases chance for challenge flow if not set)
+        /// </summary>
+        public string WorkPhoneNumber { get; set; }
     }
 }
