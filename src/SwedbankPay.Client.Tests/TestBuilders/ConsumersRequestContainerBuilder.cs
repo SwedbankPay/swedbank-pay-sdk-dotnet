276d22506a99a5c6b50d1261bc8db6efd59fd70d
Daniel Sundqvist <daniel.sundqvist@authority.se>
2019-10-23 16:13:10 +0200

    Fixed test

--- src/SwedbankPay.Client.Tests/TestBuilders/ConsumersRequestContainerBuilder.cs
+++ src/SwedbankPay.Client.Tests/TestBuilders/ConsumersRequestContainerBuilder.cs
@@ -4,7 +4,10 @@ namespace SwedbankPay.Client.Tests.TestBuilders
 {
     public class ConsumersRequestContainerBuilder
     {
-        private ConsumersRequestContainer _consumersRequestContainer = new ConsumersRequestContainer();
+        private ConsumersRequestContainer _consumersRequestContainer = new ConsumersRequestContainer
+        {
+            ConsumersRequest = new ConsumersRequest()
+        };
     
 
         public ConsumersRequestContainerBuilder WithTestValues()
