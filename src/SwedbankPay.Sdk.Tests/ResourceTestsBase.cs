1d83c6f6cd19d7b13e32d64997b293a5943dca42
Karl Fristedt <karl.fristedt@authority.se>
2019-11-13 14:40:36 +0100

    Modified urls

--- src/SwedbankPay.Sdk.Tests/ResourceTestsBase.cs
+++ src/SwedbankPay.Sdk.Tests/ResourceTestsBase.cs
@@ -13,9 +13,9 @@
             {
                 ApiBaseUrl = new Uri("https://api.externalintegration.payex.com"),
                 Token = "588431aa485611f8fce876731a1734182ca0c44fcad6b8d989e22f444104aadf",
-                CallBackUrl = new Uri("https://payex.eu.ngrok.io/payment-callback?orderGroupId={orderGroupId}"),
-                CompletePageUrl = new Uri("https://payex.eu.ngrok.io/sv/checkout-sv/PayexCheckoutConfirmation/?orderGroupId={orderGroupId}"),
-                CancelPageUrl = new Uri("https://payex.eu.ngrok.io/payment-canceled?orderGroupId={orderGroupId}"),
+                CallBackUrl = new Uri("https://www.example.com/"),
+                CompletePageUrl = new Uri("https://www.example.com/"),
+                CancelPageUrl = new Uri("https://www.example.com/"),
                 MerchantId = "91a4c8e0-72ac-425c-a687-856706f9e9a1"
             };
 
