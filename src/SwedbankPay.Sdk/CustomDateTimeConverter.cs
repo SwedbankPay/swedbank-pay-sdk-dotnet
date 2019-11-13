fb1aa75e11863bf968bfaf82afa988bfbb57ada9
Karl Fristedt <karl.fristedt@authority.se>
2019-11-13 17:28:20 +0100

    Added CustomDateTimeConverter class

---
+++ src/SwedbankPay.Sdk/CustomDateTimeConverter.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json.Converters;
+
+namespace SwedbankPay.Sdk
+{
+    public class CustomDateTimeConverter : IsoDateTimeConverter
+    {
+        public CustomDateTimeConverter()
+        {
+            base.DateTimeFormat = "yyyyMMdd";
+        }
+    }
+}
