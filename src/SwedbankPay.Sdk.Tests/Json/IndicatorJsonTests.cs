274b33e46dfe92d9c996f4d25bdbd6547a409c09
Karl Fristedt <karl.fristedt@authority.se>
2019-11-18 09:56:25 +0100

    Added tests for Indicator classes

---
+++ src/SwedbankPay.Sdk.Tests/Json/IndicatorJsonTests.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using SwedbankPay.Sdk.PaymentOrders;
+using Xunit;
+
+namespace SwedbankPay.Sdk.Tests.Json
+{
+    public class IndicatorJsonTests
+    {
+        [Fact]
+        public void CanDeSerialize_AccountAgeIndicator()
+        {
+            //ARRANGE
+
+            var jsonObject = new JObject();
+            jsonObject.Add("AccountAgeIndicator", "01");
+
+            //ACT
+            var result = JsonConvert.DeserializeObject<AccountInfo>(jsonObject.ToString());
+
+            //ASSERT
+            Assert.Equal("01", result.AccountAgeIndicator.Value);
+        }
+
+        [Fact]
+        public void CanSerialize_AccountAgeIndicator()
+        {
+            //ARRANGE
+            var accountInfo = new AccountInfo
+            {
+                AccountAgeIndicator = AccountAgeIndicator.NoAccountGuest
+            };
+            
+            //ACT
+            var result = JsonConvert.SerializeObject(accountInfo);
+            var obj = JObject.Parse(result);
+
+            var accountAgeValue = obj.GetValue(nameof(AccountAgeIndicator)).ToString();
+
+            //ASSERT
+            Assert.Equal("01", accountAgeValue);
+        }
+    }
+}
