8eec9b8dce4c50a9a88bde751df54fb942ac5b78
Daniel Sundqvist <daniel.sundqvist@authority.se>
2019-11-19 12:25:27 +0100

    Removed JsonProperty

--- src/SwedbankPay.Sdk/Consumers/ShippingDetails.cs
+++ src/SwedbankPay.Sdk/Consumers/ShippingDetails.cs
@@ -1,16 +1,11 @@
 ﻿namespace SwedbankPay.Sdk.Consumers
 {
-    using Newtonsoft.Json;
-
     public class ShippingDetails
     {
-        [JsonProperty("email")]
         public string Email { get; protected set; }
 
-        [JsonProperty("msisdn")]
         public string Msisdn { get; protected set; }
 
-        [JsonProperty("shippingAddress")]
         public Address ShippingAddress { get; protected set; }
     }
 }
