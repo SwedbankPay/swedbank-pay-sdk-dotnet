733f0a8838c2e34107483841f7c1730d91e59086
Karl Fristedt <karl.fristedt@authority.se>
2019-11-21 15:51:42 +0100

    Added JsonSerialization class

---
+++ src/SwedbankPay.Sdk/JsonSerialization/JsonSerialization.cs
@@ -0,0 +1,36 @@
+﻿namespace SwedbankPay.Sdk.JsonSerialization
+{
+    using System.Collections.Generic;
+
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+    using Newtonsoft.Json.Serialization;
+
+    using SwedbankPay.Sdk.PaymentOrders;
+
+    public class JsonSerialization
+    {
+        public static readonly JsonSerializerSettings settings = new JsonSerializerSettings
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            Converters = new List<JsonConverter>
+            {
+                new StringEnumConverter(),
+                new CustomEmailAddressConverter(typeof(EmailAddress)),
+                new TypedSafeEnumValueConverter<ShipIndicator, string>(),
+                new TypedSafeEnumValueConverter<DeliveryTimeFrameIndicator, string>(),
+                new TypedSafeEnumValueConverter<PreOrderPurchaseIndicator, string>(),
+                new TypedSafeEnumValueConverter<ReOrderPurchaseIndicator, string>(),
+                new TypedSafeEnumValueConverter<AccountAgeIndicator, string>(),
+                new TypedSafeEnumValueConverter<AccountChangeIndicator, string>(),
+                new TypedSafeEnumValueConverter<AccountPwdChangeIndicator, string>(),
+                new TypedSafeEnumValueConverter<ShippingAddressUsageIndicator, string>(),
+                new TypedSafeEnumValueConverter<ShippingNameIndicator, string>(),
+                new TypedSafeEnumValueConverter<SuspiciousAccountActivity, string>(),
+                new TypedSafeEnumValueConverter<Operation, string>()
+            },
+            NullValueHandling = NullValueHandling.Ignore,
+            DateFormatString = "yyyyMMdd"
+        };
+    }
+}
