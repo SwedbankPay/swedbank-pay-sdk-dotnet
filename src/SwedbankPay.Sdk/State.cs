5b430dfc21bda8ee9036cf0fa2580f843baa8d9d
Karl Fristedt <karl.fristedt@authority.se>
2019-11-25 10:14:05 +0100

    Added Type safe enum State class

---
+++ src/SwedbankPay.Sdk/State.cs
@@ -0,0 +1,14 @@
+﻿namespace SwedbankPay.Sdk
+{
+    public class State : TypeSafeEnum<State, string>
+    {
+        public static readonly State Initialized = new State(nameof(Initialized), "Initialized");
+        public static readonly State Completed = new State(nameof(Completed), "Completed");
+        public static readonly State Failed = new State(nameof(Failed), "Failed");
+        
+        public State(string name, string value)
+            : base(name, value)
+        {
+        }
+    }
+}
