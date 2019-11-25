1d754d7afaa6edd31602b4726fa23a2be07c268c
Karl Fristedt <karl.fristedt@authority.se>
2019-11-25 11:09:32 +0100

    Added more states, Ready, Pending and Aborted

--- src/SwedbankPay.Sdk/State.cs
+++ src/SwedbankPay.Sdk/State.cs
@@ -1,14 +1,17 @@
 ﻿namespace SwedbankPay.Sdk
 {
     /// <summary>
-    /// Initialized, Completed or Failed. Indicates the state of the transaction.
+    /// Initialized, Completed, Failed Ready, Pending or Aborted. Indicates the state.
     /// </summary>
     public class State : TypeSafeEnum<State, string>
     {
         public static readonly State Initialized = new State(nameof(Initialized), "Initialized");
         public static readonly State Completed = new State(nameof(Completed), "Completed");
         public static readonly State Failed = new State(nameof(Failed), "Failed");
-        
+        public static readonly State Ready = new State(nameof(Ready), "Ready");
+        public static readonly State Pending = new State(nameof(Pending), "Pending");
+        public static readonly State Aborted = new State(nameof(Aborted), "Aborted");
+
         public State(string name, string value)
             : base(name, value)
         {
