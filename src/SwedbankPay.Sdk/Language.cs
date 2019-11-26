c9ed21c1668f1be0a80c9feb1293df47c9d4ffc2
Karl Fristedt <karl.fristedt@authority.se>
2019-11-26 16:46:15 +0100

    Added Language class

---
+++ src/SwedbankPay.Sdk/Language.cs
@@ -0,0 +1,43 @@
+﻿namespace SwedbankPay.Sdk
+{
+    using System;
+    using System.Globalization;
+    using System.Linq;
+
+    using Newtonsoft.Json;
+    
+    public class Language
+    {
+            private string Value { get; }
+
+            [JsonConstructor]
+            public Language(string languageCode)
+            {
+                if (string.IsNullOrEmpty(languageCode))
+                {
+                    throw new ArgumentNullException(nameof(languageCode), $"Language code can't be null or empty");
+                }
+                if (!IsValidLanguage(languageCode))
+                {
+                    throw new ArgumentException($"Invalid language: {languageCode}", nameof(languageCode));
+                }
+                this.Value = languageCode;
+            }
+
+            public static bool IsValidLanguage(string languageCode)
+            {
+                if (string.IsNullOrWhiteSpace(languageCode))
+                {
+                    return false;
+                }
+                
+                return CultureInfo.GetCultures(CultureTypes.AllCultures).Any(c => c.Name.Equals(languageCode));
+            }
+
+            public override string ToString()
+            {
+                return this.Value;
+            }
+        }
+    
+}
