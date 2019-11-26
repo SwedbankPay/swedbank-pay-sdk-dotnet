1a2577016b4962972ebaae071ce99953f408d4d1
Karl Fristedt <karl.fristedt@authority.se>
2019-11-26 15:09:28 +0100

    Added conditional tls12

--- src/SwedbankPay.Sdk/SwedbankPayClient.cs
+++ src/SwedbankPay.Sdk/SwedbankPayClient.cs
@@ -20,8 +20,11 @@
                                  HttpClient httpClient,
                                  ILogger logger = null)
         {
+            if (!ServicePointManager.SecurityProtocol.HasFlag(SecurityProtocolType.Tls12))
+            {
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            }
 
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
             var swedbankLogger = logger ?? NullLogger.Instance;
             PaymentOrders = new PaymentOrdersResource(swedbankPayOptions, swedbankLogger, httpClient);
             Payment = new PaymentsResource(swedbankPayOptions, swedbankLogger, httpClient);
