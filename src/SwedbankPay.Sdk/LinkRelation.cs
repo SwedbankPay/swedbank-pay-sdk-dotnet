b388bfb1276ba3b765f9597e59ed5823eb3e3e7e
Karl Fristedt <karl.fristedt@authority.se>
2019-12-03 13:22:38 +0100

    Added LinkRelation class

---
+++ src/SwedbankPay.Sdk/LinkRelation.cs
@@ -0,0 +1,23 @@
+﻿#region License
+
+// --------------------------------------------------
+// Copyright © Swedbank Pay. All Rights Reserved.
+// 
+// This software is proprietary information of Swedbank Pay.
+// USE IS SUBJECT TO LICENSE TERMS.
+// --------------------------------------------------
+
+#endregion
+
+namespace SwedbankPay.Sdk
+{
+    public sealed class LinkRelation : TypeSafeEnum<LinkRelation, string>
+    {
+        public static readonly LinkRelation UpdatePaymentorderUpdateorder = new LinkRelation(nameof(UpdatePaymentorderUpdateorder), "update-paymentorder-updateorder");
+        public static readonly LinkRelation CreatePaymentorderCapture = new LinkRelation(nameof(CreatePaymentorderCapture), "create-paymentorder-capture");
+        public static readonly LinkRelation CreatePaymentorderCancellation = new LinkRelation(nameof(CreatePaymentorderCancellation), "create-paymentorder-cancellation");
+        public LinkRelation(string name, string value) : base(name, value)
+        {
+        }
+    }
+}
