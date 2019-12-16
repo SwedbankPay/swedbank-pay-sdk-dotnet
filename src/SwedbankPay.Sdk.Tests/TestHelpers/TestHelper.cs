1b3097e404f0870535140caf5dce1cb5f44200ad
Karl Fristedt <karl.fristedt@authority.se>
2019-12-16 13:24:22 +0100

    testing

--- src/SwedbankPay.Sdk.Tests/TestHelpers/TestHelper.cs
+++ src/SwedbankPay.Sdk.Tests/TestHelpers/TestHelper.cs
@@ -15,6 +15,18 @@ namespace SwedbankPay.Sdk.Tests.TestHelpers
                 .SetBasePath(outputPath)
                 .AddJsonFile("appsettings.json", true)
                 .AddUserSecrets("55739ea0-5447-45e4-b35e-e0412f172f5f")
+                .AddEnvironmentVariables()
+                .AddInMemoryCollection(new Dictionary<string, string>
+                {
+                    { "SwedbankPayConnectionSettings:Token", Environment.GetEnvironmentVariable("SwedbankPayConnectionSettings:Token")},
+                    { "SwedbankPayConnectionSettings:ApiBaseUrl", Environment.GetEnvironmentVariable("SwedbankPayConnectionSettings:ApiBaseUrl") },
+                    { "Urls:TermsOfServiceUrl", Environment.GetEnvironmentVariable("Urls:TermsOfServiceUrl") },
+                    { "Urls:CallBackUrl", Environment.GetEnvironmentVariable("Urls:CallBackUrl") },
+                    { "Urls:CancelUrl", Environment.GetEnvironmentVariable("Urls:CancelUrl") },
+                    { "Urls:CompleteUrl", Environment.GetEnvironmentVariable("Urls:CompleteUrl") },
+                    { "Urls:LogoUrl", Environment.GetEnvironmentVariable("Urls:LogoUrl") },
+                    { "Urls:HostUrls:0", Environment.GetEnvironmentVariable("Urls:HostUrls:0") }
+                })
                 .Build();
 
 #elif RELEASE
@@ -43,7 +55,7 @@ namespace SwedbankPay.Sdk.Tests.TestHelpers
         public static SwedbankPayConnectionSettings GetSwedbankPayConnectionSettings(string outputPath)
         {
             var configuration = new SwedbankPayConnectionSettings();
-
+           
             var iConfig = GetIConfigurationRoot(outputPath);
 
             iConfig
@@ -57,6 +69,10 @@ namespace SwedbankPay.Sdk.Tests.TestHelpers
         public static Urls GetUrls(string outputPath)
         {
             var configuration = new UrlsOptions();
+            Environment.SetEnvironmentVariable("Urls:CompleteUrl", "https://www.example.com");
+            Environment.SetEnvironmentVariable("Urls:HostUrls:0", "https://www.example2.com");
+            Environment.SetEnvironmentVariable("Urls:TermsOfServiceUrl", "https://www.example2.com");
+            Environment.SetEnvironmentVariable("Urls:CallBackUrl", "https://www.example2.com");
             var iConfig = GetIConfigurationRoot(outputPath);
 
             iConfig
