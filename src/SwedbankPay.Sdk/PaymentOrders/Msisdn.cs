e48e40662b43f80cf395844b03b19765fb95b050
Daniel Sundqvist <daniel.sundqvist@authority.se>
2019-12-19 11:53:35 +0100

    Removed commented out code

--- src/SwedbankPay.Sdk/PaymentOrders/Msisdn.cs
+++ src/SwedbankPay.Sdk/PaymentOrders/Msisdn.cs
@@ -23,42 +23,6 @@ namespace SwedbankPay.Sdk.PaymentOrders
             if (string.IsNullOrWhiteSpace(msisdn))
                 return false;
             return true;
-            //try
-            //{
-            //    // Normalize the domain
-            //    emailAddress = Regex.Replace(emailAddress, @"(@)(.+)$", DomainMapper,
-            //        RegexOptions.None, TimeSpan.FromMilliseconds(200));
-
-            //    // Examines the domain part of the email and normalizes it.
-            //    string DomainMapper(Match match)
-            //    {
-            //        // Use IdnMapping class to convert Unicode domain names.
-            //        var idn = new IdnMapping();
-
-            //        // Pull out and process domain name (throws ArgumentException on invalid)
-            //        var domainName = idn.GetAscii(match.Groups[2].Value);
-
-            //        return match.Groups[1].Value + domainName;
-            //    }
-            //}
-            //catch (RegexMatchTimeoutException e)
-            //{
-            //    return false;
-            //}
-            //catch (ArgumentException e)
-            //{
-            //    return false;
-            //}
-
-            //try
-            //{
-            //    return Regex.IsMatch(msisdn, @"/^46[1-9][0-9]{8,12}$/",
-            //        RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
-            //}
-            //catch (RegexMatchTimeoutException)
-            //{
-            //    return false;
-            //}
         }
 
 
