aae12e68ee09db26a80d29ecaee4c6f2d40d5e5c
Daniel Sundqvist <daniel.sundqvist@authority.se>
2019-12-22 22:38:00 +0100

    Made classes immutable

--- src/SwedbankPay.Sdk/PaymentOrders/CurrentPaymentResponseContainer.cs
+++ src/SwedbankPay.Sdk/PaymentOrders/CurrentPaymentResponseContainer.cs
@@ -6,14 +6,22 @@ namespace SwedbankPay.Sdk.PaymentOrders
 {
     public class CurrentPaymentResponseContainer : PaymentResponseContainer
     {
+        public CurrentPaymentResponseContainer(Uri id, string menuElementName,
+                                               OperationList operations,
+                                               PaymentResponse payment) : base(operations, payment)
+        {
+            Id = id;
+            MenuElementName = menuElementName;
+        }
+
         /// <summary>
         ///     The relative URI to the payment.
         /// </summary>
-        public Uri Id { get; set; }
+        public Uri Id { get; }
 
         /// <summary>
         ///     creditcard, invoice, etc. The name of the selected menu element.
         /// </summary>
-        public string MenuElementName { get; set; }
+        public string MenuElementName { get; }
     }
 }
--- src/SwedbankPay.Sdk/PaymentOrders/PaymentOrderResponse.cs
+++ src/SwedbankPay.Sdk/PaymentOrders/PaymentOrderResponse.cs
@@ -4,26 +4,51 @@ namespace SwedbankPay.Sdk.PaymentOrders
 {
     public class PaymentOrderResponse : IdLink
     {
-        public Amount Amount { get; set; }
-        public DateTime Created { get; set; }
-        public CurrencyCode Currency { get; set; }
-        public CurrentPaymentResponseContainer CurrentPayment { get; set; }
-        public string Description { get; set; }
-        public Language Language { get; set; }
-        public MetaDataContainer MetaData { get; set; }
-        public string Operation { get; set; }
-        public OrderItems OrderItems { get; set; }
-        public PayeeInfo PayeeInfo { get; set; }
-        public Payer Payers { get; set; }
-        public IdLink Payments { get; set; }
-        public Amount RemainingCancellationAmount { get; set; }
-        public Amount RemainingCaptureAmount { get; set; }
-        public Amount RemainingReversalAmount { get; set; }
-        public IdLink Settings { get; set; }
-        public State State { get; set; }
-        public DateTime Updated { get; set; }
-        public Urls Urls { get; set; }
-        public string UserAgent { get; set; }
-        public Amount VatAmount { get; set; }
+        public PaymentOrderResponse(Amount amount, DateTime created, CurrencyCode currency, CurrentPaymentResponseContainer currentPayment, string description, Language language, MetaDataContainer metaData, string operation, OrderItems orderItems, PayeeInfo payeeInfo, Payer payers, IdLink payments, Amount remainingCancellationAmount, Amount remainingCaptureAmount, Amount remainingReversalAmount, IdLink settings, State state, DateTime updated, Urls urls, string userAgent, Amount vatAmount)
+        {
+            Amount = amount;
+            Created = created;
+            Currency = currency;
+            CurrentPayment = currentPayment;
+            Description = description;
+            Language = language;
+            MetaData = metaData;
+            Operation = operation;
+            OrderItems = orderItems;
+            PayeeInfo = payeeInfo;
+            Payers = payers;
+            Payments = payments;
+            RemainingCancellationAmount = remainingCancellationAmount;
+            RemainingCaptureAmount = remainingCaptureAmount;
+            RemainingReversalAmount = remainingReversalAmount;
+            Settings = settings;
+            State = state;
+            Updated = updated;
+            Urls = urls;
+            UserAgent = userAgent;
+            VatAmount = vatAmount;
+        }
+
+        public Amount Amount { get; }
+        public DateTime Created { get; }
+        public CurrencyCode Currency { get; }
+        public CurrentPaymentResponseContainer CurrentPayment { get; }
+        public string Description { get; }
+        public Language Language { get; }
+        public MetaDataContainer MetaData { get; }
+        public string Operation { get; }
+        public OrderItems OrderItems { get; }
+        public PayeeInfo PayeeInfo { get; }
+        public Payer Payers { get; }
+        public IdLink Payments { get; }
+        public Amount RemainingCancellationAmount { get; }
+        public Amount RemainingCaptureAmount { get; }
+        public Amount RemainingReversalAmount { get; }
+        public IdLink Settings { get; }
+        public State State { get; }
+        public DateTime Updated { get; }
+        public Urls Urls { get; }
+        public string UserAgent { get; }
+        public Amount VatAmount { get; }
     }
 }
