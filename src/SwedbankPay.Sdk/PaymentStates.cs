cfea3d3e6039e3504a05c07f8a3b1cd9d0e07b9f
Daniel Sundqvist <daniel.sundqvist@authority.se>
2020-01-08 11:34:14 +0100

    Removed unused class

--- src/SwedbankPay.Sdk/PaymentStates.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SwedbankPay.Sdk
-{
-    public static class PaymentStates
-    {
-        public const string Aborted = "Aborted";
-        public const string Completed = "Completed";
-        public const string AwaitingActivity = "AwaitingActivity";
-    }
-}
