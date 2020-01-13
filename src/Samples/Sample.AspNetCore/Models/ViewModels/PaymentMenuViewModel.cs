f3a6081c3670101b3a99b506eaf1e7f8c8e770b4
Karl Fristedt <karl.fristedt@authority.se>
2020-01-13 13:48:24 +0100

    Added viewmodel

---
+++ src/Samples/Sample.AspNetCore/Models/ViewModels/PaymentMenuViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SwedbankPay.Sdk;
+
+namespace Sample.AspNetCore.Models.ViewModels
+{
+    public class PaymentMenuViewModel
+    {
+        public SwedbankPay.Sdk.Payments.Card.Operations CardOperations { get; set; }
+        public SwedbankPay.Sdk.Payments.Swish.Operations SwishOperations { get; set; }
+        public string PaymentLink { get; set; }
+        public string JsSource { get; set; }
+    }
+}
