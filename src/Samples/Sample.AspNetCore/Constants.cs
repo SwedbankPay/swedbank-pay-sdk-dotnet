3f546f27c95f8dd586ce66141d92390e4a3ea652
Daniel Sundqvist <daniel.sundqvist@authority.se>
2020-01-13 08:59:02 +0100

    Moved sample project

--- src/Samples/Sample.AspNetCore/Constants.cs
+++
@@ -1,7 +0,0 @@
-namespace Sample.AspNetCore
-{
-    public class Constants
-    {
-        public const string Someaccount = "someAccount";
-    }
-}
--- src/Samples/Sample.AspNetCore/Startup.cs
+++ src/Samples/Sample.AspNetCore/Startup.cs
@@ -1,11 +1,17 @@
-﻿namespace Sample.AspNetCore
-{
-    using Microsoft.AspNetCore.Builder;
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.AspNetCore.Http;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.DependencyInjection;
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Sample.AspNetCore.Data;
+using Sample.AspNetCore.Extensions;
+using Sample.AspNetCore.Models;
 
+namespace Sample.AspNetCore
+{
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -13,19 +19,57 @@
             Configuration = configuration;
         }
 
+
         public IConfiguration Configuration { get; }
 
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    "default",
+                    "{controller=Products}/{action=Index}/{id?}");
+            });
+        }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<StoreDbContext>(options => options.UseInMemoryDatabase("Products"));
+            services.AddControllersWithViews();
+            services.AddDistributedMemoryCache();
+            Guid.TryParse(Configuration.GetSection("PayeeInfo")["PayeeId"], out Guid payeeId);
+            services.Configure<PayeeInfoConfig>(options =>
+            {
+                options.PayeeId = payeeId;
+                options.PayeeReference = DateTime.Now.Ticks.ToString();
+            });
+            services.Configure<UrlsOptions>(Configuration.GetSection("Urls"));
+            services.AddScoped(provider => SessionCart.GetCart(provider));
             services.AddTransient<IHttpContextAccessor, HttpContextAccessor>();
-            services.AddSwedbankPayClient(Configuration, Constants.Someaccount);
-            services.AddMvc();
-        }
-        
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            app.UseDeveloperExceptionPage();
-            app.UseMvc();
+            services.AddSwedbankPayClient(Configuration);
+            services.AddSession();
         }
     }
 }
