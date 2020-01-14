af12bb9ee348c06976b4df991c11fcb8260e1782
Karl Fristedt <karl.fristedt@authority.se>
2020-01-14 13:33:46 +0100

    Made immutable

--- src/SwedbankPay.Sdk/PaymentOrders/NationalIdentifier.cs
+++ src/SwedbankPay.Sdk/PaymentOrders/NationalIdentifier.cs
@@ -7,16 +7,21 @@ namespace SwedbankPay.Sdk.PaymentOrders
 {
     public class NationalIdentifier
     {
+        public NationalIdentifier(CountryCode countryCode, string socialSecurityNumber)
+        {
+            CountryCode = countryCode;
+            SocialSecurityNumber = socialSecurityNumber;
+        }
+        
         /// <summary>
         ///     The country code, denoting origin for the issued social security number. Required if
         ///     nationalIdentifier.socialSecurityNumber is set.
         /// </summary>
-        [JsonConverter(typeof(StringEnumConverter))]
-        public CountryCode CountryCode { get; set; }
+        public CountryCode CountryCode { get; }
 
         /// <summary>
         ///     The social security number of the payer. Format: Norway DDMMYYXXXXX, Sweden: YYYYMMDDXXXX.
         /// </summary>
-        public string SocialSecurityNumber { get; set; }
+        public string SocialSecurityNumber { get; }
     }
 }
