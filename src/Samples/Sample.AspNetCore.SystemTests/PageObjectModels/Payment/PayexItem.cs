76f3b102b9e4da7aa6a00917b5db3b2c068ec099
Daniel Granerud <daniel.granerud@payex.com>
2020-01-31 09:32:20 +0100

    More clean up of visual studio messages

---
+++ src/Samples/Sample.AspNetCore.SystemTests/PageObjectModels/Payment/PayexItem.cs
@@ -0,0 +1,15 @@
+﻿using Atata;
+
+namespace Sample.AspNetCore.SystemTests.PageObjectModels
+{
+    using _ = PaymentFramePage;
+
+    [ControlDefinition("div", ContainingClass = "custom-menu-card")]
+    public class PayexItem : Control<_>
+    {
+        [FindByClass("menu-card-title")] public Text<_> Name { get; private set; }
+
+        [WaitFor(Until.Visible, TriggerEvents.BeforeAccess)]
+        public Frame<_> PaymentFrame { get; private set; }
+    }
+}
