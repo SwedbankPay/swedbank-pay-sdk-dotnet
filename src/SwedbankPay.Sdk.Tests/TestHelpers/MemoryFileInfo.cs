680bfeb82b4c48fd86755f16582b50ab5b448d6d
Daniel Granerud <daniel.granerud@payex.com>
2020-02-04 11:32:00 +0100

    Revert "Delete unused files"
    This reverts commit af723bebdc8602483650b3cbaa72ee93ac14c467.

---
+++ src/SwedbankPay.Sdk.Tests/TestHelpers/MemoryFileInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.FileProviders;
+
+namespace SwedbankPay.Sdk.Tests.TestHelpers
+{
+    public class MemoryFileInfo : IFileInfo
+    {
+        private readonly byte[] _content;
+
+        public MemoryFileInfo(string name, byte[] content, DateTimeOffset timestamp)
+        {
+            Name = name;
+            this._content = content;
+            LastModified = timestamp;
+        }
+
+        public bool Exists => true;
+
+        long IFileInfo.Length => this._content.LongLength;
+
+        public string PhysicalPath => null;
+
+        public string Name { get; }
+
+        public DateTimeOffset LastModified { get; }
+
+        public bool IsDirectory => false;
+
+        public Stream CreateReadStream()
+        {
+            return new MemoryStream(this._content);
+        }
+    }
+}
