f50ef9ea6d86ef2d4a4a184fa92fda6a3b56fd3c
Daniel Granerud <daniel.granerud@payex.com>
2020-02-05 15:58:15 +0100

    Improve IServiceCollection extension

--- src/SwedbankPay.Sdk/Extensions/ServiceCollectionExtensions.cs
+++ src/SwedbankPay.Sdk/Extensions/ServiceCollectionExtensions.cs
@@ -15,5 +15,19 @@ namespace SwedbankPay.Sdk.Extensions
         {
             return services.AddHttpClient(nameof(SwedbankPayClient), configureClient);
         }
+
+        public static IHttpClientBuilder AddSwedbankPayClient(this IServiceCollection services, Uri baseAddress, string authenticationToken)
+        {
+            if (string.IsNullOrWhiteSpace(authenticationToken ))
+                throw new ArgumentNullException(nameof(authenticationToken));
+            if (Uri.IsWellFormedUriString(baseAddress.AbsolutePath, UriKind.Absolute) == false)
+                throw new ArgumentException($"{nameof(baseAddress)} is not a well formed {typeof(Uri)} string.");
+
+            return services.AddHttpClient(nameof(SwedbankPayClient), a =>
+            {
+                a.BaseAddress = baseAddress;
+                a.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer",authenticationToken);
+            });
+        }
     }
 }
