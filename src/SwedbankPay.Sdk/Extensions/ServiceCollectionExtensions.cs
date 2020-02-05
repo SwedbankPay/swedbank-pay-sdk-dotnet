9c50ee71efdac5b4ed1e340d9f2f65bf2dc02025
Daniel Granerud <daniel.granerud@payex.com>
2020-02-05 13:55:45 +0100

    Add helper to set up swedbankpay client

---
+++ src/SwedbankPay.Sdk/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+
+namespace SwedbankPay.Sdk.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IHttpClientBuilder AddSwedbankPayClient(this IServiceCollection services, Action<HttpClient> configureClient = null)
+        {
+            if(configureClient != null)
+                return services.AddHttpClient(nameof(SwedbankPayClient), configureClient);
+            return services.AddHttpClient(nameof(SwedbankPayClient));
+        }
+
+        public static IHttpClientBuilder AddSwedbankPayClient(this IServiceCollection services, Action<IServiceProvider, HttpClient> configureClient = null)
+        {
+            if (configureClient != null)
+                return services.AddHttpClient(nameof(SwedbankPayClient), configureClient);
+            return services.AddHttpClient(nameof(SwedbankPayClient));
+        }
+    }
+}
