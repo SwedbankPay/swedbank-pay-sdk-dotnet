a7d8f251c9ecf361266eea18870bca4189496269
Trieu Tran <trieutran91@users.noreply.github.com>
2020-02-10 15:02:14 +0100

    DX-687 Edited authorization

--- src/SwedbankPay.Sdk/Payments/Invoice/Authorization.cs
+++ src/SwedbankPay.Sdk/Payments/Invoice/Authorization.cs
@@ -4,74 +4,15 @@ namespace SwedbankPay.Sdk.Payments.Invoice
 {
     public class Authorization : IdLink
     {
-        public Authorization(bool direct,
-                             string paymentToken,
-                             string recurrenceToken,
-                             string maskedPan,
-                             string expiryDate,
-                             string panToken,
-                             string cardBrand,
-                             string cardType,
-                             string issuingBank,
-                             string countryCode,
-                             string acquirerTransactionType,
-                             string issuerAuthorizationApprovalCode,
-                             string acquirerStan,
-                             string acquirerTerminalId,
-                             DateTime acquirerTransactionTime,
-                             string authenticationStatus,
-                             string nonPaymentToken,
-                             string externalNonPaymentToken,
-                             string externalSiteId,
-                             string transactionInitiator,
+        public Authorization(
                              AuthorizationTransaction transaction)
         {
-            Direct = direct;
-            PaymentToken = paymentToken;
-            RecurrenceToken = recurrenceToken;
-            MaskedPan = maskedPan;
-            ExpiryDate = expiryDate;
-            PanToken = panToken;
-            CardBrand = cardBrand;
-            CardType = cardType;
-            IssuingBank = issuingBank;
-            CountryCode = countryCode;
-            AcquirerTransactionType = acquirerTransactionType;
-            IssuerAuthorizationApprovalCode = issuerAuthorizationApprovalCode;
-            AcquirerStan = acquirerStan;
-            AcquirerTerminalId = acquirerTerminalId;
-            AcquirerTransactionTime = acquirerTransactionTime;
-            AuthenticationStatus = authenticationStatus;
-            NonPaymentToken = nonPaymentToken;
-            ExternalNonPaymentToken = externalNonPaymentToken;
-            ExternalSiteId = externalSiteId;
-            TransactionInitiator = transactionInitiator;
             Transaction = transaction;
         }
 
 
-        public string AcquirerStan { get; }
-        public string AcquirerTerminalId { get; }
-        public DateTime AcquirerTransactionTime { get; }
-        public string AcquirerTransactionType { get; }
-        public string AuthenticationStatus { get; }
-        public string CardBrand { get; }
-        public string CardType { get; }
-        public string CountryCode { get; }
-
-        public bool Direct { get; }
-        public string ExpiryDate { get; }
-        public string ExternalNonPaymentToken { get; }
-        public string ExternalSiteId { get; }
-        public string IssuerAuthorizationApprovalCode { get; }
-        public string IssuingBank { get; }
-        public string MaskedPan { get; }
-        public string NonPaymentToken { get; }
-        public string PanToken { get; }
-        public string PaymentToken { get; }
-        public string RecurrenceToken { get; }
+      
         public AuthorizationTransaction Transaction { get; }
-        public string TransactionInitiator { get; }
     }
 
     public class AuthorizationTransaction : IdLink
