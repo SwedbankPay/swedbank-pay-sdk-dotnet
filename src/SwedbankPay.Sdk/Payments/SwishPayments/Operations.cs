b9532cef9a6b14bcb5c141fe5497441364c6d042
Daniel Granerud <daniel.granerud@payex.com>
2020-02-11 11:38:15 +0100

    Forgot to save files

--- src/SwedbankPay.Sdk/Payments/SwishPayments/Operations.cs
+++ src/SwedbankPay.Sdk/Payments/SwishPayments/Operations.cs
@@ -7,8 +7,8 @@ namespace SwedbankPay.Sdk.Payments.SwishPayments
     public class Operations : Dictionary<LinkRelation, HttpOperation>
     {
         public Func<Task<PaymentResponse>> Abort { get; internal set; }
-        public Func<ReversalRequest, Task<ReversalResponse>> CreateReversal { get; internal set; }
-        public Func<SaleRequest, Task<SaleResponse>> CreateSale { get; internal set; }
+        public Func<ReversalRequest, Task<ReversalResponse>> Reversal { get; internal set; }
+        public Func<SaleRequest, Task<SaleResponse>> Sale { get; internal set; }
         public new HttpOperation this[LinkRelation rel] => ContainsKey(rel) ? base[rel] : null;
         public HttpOperation PaidPayment { get; internal set; }
         public HttpOperation RedirectSale { get; internal set; }
