2f971088c23feb10cbe7654920bde727dfa8837c
Daniel Granerud <daniel.granerud@payex.com>
2020-02-12 15:10:52 +0100

    Create and use OperationsBase class to avoid using `new` to hide index accessor

--- src/SwedbankPay.Sdk/Payments/SwishPayments/SwishPaymentOperations.cs
+++ src/SwedbankPay.Sdk/Payments/SwishPayments/SwishPaymentOperations.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace SwedbankPay.Sdk.Payments.SwishPayments
 {
-    public class SwishPaymentOperations : Dictionary<LinkRelation, HttpOperation>
+    public class SwishPaymentOperations : OperationsBase
     {
         public Func<Task<SwishPaymentPaymentResponse>> Abort { get; internal set; }
         public Func<SwishPaymentReversalRequest, Task<ReversalResponse>> Reverse { get; internal set; }
         public Func<SwishPaymentSaleRequest, Task<SwishPaymentSaleResponse>> Sale { get; internal set; }
-        public new HttpOperation this[LinkRelation rel] => ContainsKey(rel) ? base[rel] : null;
         public HttpOperation PaidPayment { get; internal set; }
         public HttpOperation RedirectSale { get; internal set; }
         public HttpOperation ViewSales { get; internal set; }
