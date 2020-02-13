ab24de6a7a9d5d02762287d90aaaf6ee7a819b16
Daniel Granerud <daniel.granerud@payex.com>
2020-02-13 13:07:06 +0100

    Give SwedbankPayClient a constructor taking one parameter

--- src/SwedbankPay.Sdk/SwedbankPayClient.cs
+++ src/SwedbankPay.Sdk/SwedbankPayClient.cs
@@ -5,6 +5,8 @@ using System.Net.Http;
 using SwedbankPay.Sdk.Consumers;
 using SwedbankPay.Sdk.Payments;
 using SwedbankPay.Sdk.PaymentOrders;
+using SwedbankPay.Sdk.Payments.SwishPayments;
+using SwedbankPay.Sdk.Payments.CardPayments;
 
 namespace SwedbankPay.Sdk
 {
@@ -16,10 +18,8 @@ namespace SwedbankPay.Sdk
         {
             if (!ServicePointManager.SecurityProtocol.HasFlag(SecurityProtocolType.Tls12))
                 ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12;
-            if (httpClient == null)
-                throw new ArgumentNullException(nameof(httpClient));
 
-            this.httpClient = httpClient;
+            this.httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
 
             if (this.httpClient.BaseAddress == null)
                 throw new ArgumentNullException(nameof(httpClient), $"{nameof(httpClient.BaseAddress)} cannot be null.");
@@ -30,6 +30,27 @@ namespace SwedbankPay.Sdk
             PaymentOrders = paymentOrders;
             Consumers = consumers;
             Payments = payments;
+        }
+
+        public SwedbankPayClient(HttpClient httpClient)
+        {
+            if (!ServicePointManager.SecurityProtocol.HasFlag(SecurityProtocolType.Tls12))
+                ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12;
+
+            this.httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+
+            if (this.httpClient.BaseAddress == null)
+                throw new ArgumentNullException(nameof(httpClient), $"{nameof(httpClient.BaseAddress)} cannot be null.");
+
+            if (this.httpClient.DefaultRequestHeaders?.Authorization?.Parameter == null)
+                throw new ArgumentException($"Please configure the {nameof(httpClient)} with an Authorization header.");
+
+            var cardPaymentsResource = new CardPaymentsResource(this.httpClient);
+            var swishPaymentsResource = new SwishPaymentsResource(this.httpClient);
+
+            PaymentOrders = new PaymentOrdersResource(this.httpClient);
+            Consumers = new ConsumersResource(this.httpClient);
+            Payments = new PaymentsResource(this.httpClient, cardPaymentsResource, swishPaymentsResource);
         }
 
         public IPaymentOrdersResource PaymentOrders { get; }
