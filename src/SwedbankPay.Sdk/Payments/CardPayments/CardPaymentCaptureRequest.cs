ce548fad735ec8a9641caa243d21b5a480850182
Daniel Granerud <daniel.granerud@payex.com>
2020-02-13 11:11:41 +0100

    Make request classes private and nested

--- src/SwedbankPay.Sdk/Payments/CardPayments/CardPaymentCaptureRequest.cs
+++ src/SwedbankPay.Sdk/Payments/CardPayments/CardPaymentCaptureRequest.cs
@@ -8,10 +8,40 @@ namespace SwedbankPay.Sdk.Payments.CardPayments
     {
         public CardPaymentCaptureRequest(Amount amount, Amount vatAmount, List<OrderItem> orderItems, string description, string payeeReference)
         {
-            Transaction = new CardPaymentCaptureTransaction(amount, vatAmount, orderItems, description, payeeReference);
+            transaction = new CardPaymentCaptureTransaction(amount, vatAmount, orderItems, description, payeeReference);
         }
 
+        private CardPaymentCaptureTransaction transaction { get; }
 
-        public CardPaymentCaptureTransaction Transaction { get; }
+
+        public Amount Amount => this.transaction.Amount;
+        public string Description => this.transaction.Description;
+        public List<OrderItem> OrderItems => this.transaction.OrderItems;
+        public string PayeeReference => this.transaction.PayeeReference;
+        public Amount VatAmount => this.transaction.VatAmount;
+
+
+        private class CardPaymentCaptureTransaction
+        {
+            protected internal CardPaymentCaptureTransaction(Amount amount,
+                                                    Amount vatAmount,
+                                                    List<OrderItem> orderItems,
+                                                    string description,
+                                                    string payeeReference)
+            {
+                Amount = amount;
+                VatAmount = vatAmount;
+                OrderItems = orderItems;
+                Description = description;
+                PayeeReference = payeeReference;
+            }
+
+
+            public Amount Amount { get; }
+            public string Description { get; }
+            public List<OrderItem> OrderItems { get; }
+            public string PayeeReference { get; }
+            public Amount VatAmount { get; }
+        }
     }
 }
