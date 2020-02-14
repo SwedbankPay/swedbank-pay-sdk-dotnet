b6a1243da545e4767acc67f3e68b8770cf98e22b
Daniel Granerud <daniel.granerud@payex.com>
2020-02-14 11:44:46 +0100

    Create a smaller constructor for PaymentsResource

--- src/SwedbankPay.Sdk/Payments/PaymentsResource.cs
+++ src/SwedbankPay.Sdk/Payments/PaymentsResource.cs
@@ -10,6 +10,12 @@ namespace SwedbankPay.Sdk.Payments
     {
         public ICardPaymentsResource CardPayments { get; private set ; }
         public ISwishPaymentsResource SwishPayments { get; private set; }
+        
+        public PaymentsResource(HttpClient httpClient) : base(httpClient)
+        {
+            this.CardPayments = new CardPaymentsResource(httpClient);
+            this.SwishPayments = new SwishPaymentsResource(httpClient);
+        }
 
         public PaymentsResource(HttpClient httpClient, ICardPaymentsResource cardPaymentsResource, ISwishPaymentsResource swishPaymentsResource) : base(httpClient)
         {
