7b7f938b4c716029b0877e6cbf5bf86fe1107bf9
Daniel Granerud <daniel.granerud@payex.com>
2020-02-17 10:25:06 +0100

    Clean up sample project

--- src/Samples/Sample.AspNetCore/Extensions/ServiceCollectionExtensions.cs
+++ src/Samples/Sample.AspNetCore/Extensions/ServiceCollectionExtensions.cs
@@ -1,21 +1,11 @@
-﻿using System;
-
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-using Polly;
-
-using SwedbankPay.Sdk.Extensions;
 
 using Sample.AspNetCore.Models;
 
 using SwedbankPay.Sdk;
 using System.Net.Http;
-using SwedbankPay.Sdk.Payments.CardPayments;
-using SwedbankPay.Sdk.Payments.SwishPayments;
-using SwedbankPay.Sdk.PaymentOrders;
-using SwedbankPay.Sdk.Consumers;
-using SwedbankPay.Sdk.Payments;
 using System.Net.Http.Headers;
 
 namespace Sample.AspNetCore.Extensions
@@ -29,7 +19,7 @@ namespace Sample.AspNetCore.Extensions
             services.Configure<SwedbankPayConnectionSettings>(swedbankPayConSettings);
 
             var swedBankPayOptions = swedbankPayConSettings.Get<SwedbankPayConnectionSettings>();
-            swedBankPayOptions.Token = "6430eee8fe8a2902f74494087ae79b9ff2e2f9d1efd3eeac1110e79d3502df24";
+            swedBankPayOptions.Token = configuration["Token"];
 
             services.AddTransient(s => swedBankPayOptions);
 
