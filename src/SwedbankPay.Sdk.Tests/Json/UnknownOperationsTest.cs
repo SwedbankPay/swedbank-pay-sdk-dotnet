929d750aeefd3ef6e01f2abbddad0ebde24eeda3
Daniel Granerud <daniel.granerud@payex.com>
2020-02-19 10:24:15 +0100

    Add test for deserializing unknonwn operation to LinkRelation

--- src/SwedbankPay.Sdk.Tests/Json/UnknownOperationsTest.cs
+++ src/SwedbankPay.Sdk.Tests/Json/UnknownOperationsTest.cs
@@ -1,8 +1,5 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json;
 using SwedbankPay.Sdk.Payments.CardPayments;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using Xunit;
 
 namespace SwedbankPay.Sdk.Tests.Json
@@ -12,43 +9,66 @@ namespace SwedbankPay.Sdk.Tests.Json
         [Fact]
         public void CanDeserializeUnknownOperation()
         {
-
+            var paymentResponse = JsonConvert.DeserializeObject<CardPaymentResponse>(TestResponse, JsonSerialization.JsonSerialization.Settings);
+            Assert.NotNull(paymentResponse);
         }
 
-        public static Uri LocalHost = new Uri("localhost:4040");
-        public static CardPaymentResponse TestResponse = new CardPaymentResponse(
-            new PaymentResponseObject(
-                    LocalHost,
-                    "number",
-                    PaymentInstrument.CreditCard,
-                    DateTime.UtcNow,
-                    DateTime.UtcNow,
-                    State.Initialized,
-                    Operation.Initiate,
-                    Payments.Intent.Authorization,
-                    new CurrencyCode("sek"),
-                    Amount.FromInt(1337),
-                    Amount.FromInt(0),
-                    Amount.FromInt(0),
-                    Amount.FromInt(0),
-                    "description",
-                    "payerReference",
-                    "initiatingSystemUserAgent",
-                    "userAgent",
-                    System.Globalization.CultureInfo.InvariantCulture,
-                    new Payments.PricesListResponse(new List<Payments.Price>()),
-                    new Payments.TransactionListResponse(, new List<Payments.Transaction>()),
-                    new CardPaymentAuthorizationListResponse(LocalHost, new List<CardPaymentAuthorization>()),
-                    new Payments.CapturesListResponse(LocalHost, new List<Payments.TransactionResponse>()),
-                    new Payments.ReversalsListResponse(LocalHost, new List<Payments.TransactionResponse>()),
-                    new Payments.CancellationsListResponse(LocalHost, new List<Payments.TransactionResponse>()),
-                    new Urls(LocalHost ,new List<Uri>()),
-                    new PayeeInfo(Guid.Empty, "payeeReference"),
-                    new PaymentOrders.MetaDataResponse(new Dictionary<string, object>())
-                ),
-            new OperationList(new List<HttpOperation>
-            {
-                new HttpOperation(LocalHost, new LinkRelation("test", "test-unknown"), "GET", "text/test")
-            }));
+        [Fact]
+        public void CanAccessDeserializedUnknownOperation()
+        {
+            var paymentResponse = JsonConvert.DeserializeObject<CardPaymentResponse>(TestResponse, JsonSerialization.JsonSerialization.Settings);
+            //paymentResponse.Operations["test"];
+        }
+
+        public static string TestResponse = @"{
+    ""payment"": {
+        ""id"": ""/psp/creditcard/payments/07f14eae-ee4d-414d-2635-08d7b43fe32e"",
+        ""number"": 70100358890,
+        ""created"": ""2020-02-19T07:49:05.4375875Z"",
+        ""updated"": ""2020-02-19T07:49:20.2673946Z"",
+        ""instrument"": ""CreditCard"",
+        ""operation"": ""Purchase"",
+        ""intent"": ""Authorization"",
+        ""state"": ""Ready"",
+        ""currency"": ""NOK"",
+        ""prices"": {
+            ""id"": ""/psp/creditcard/payments/07f14eae-ee4d-414d-2635-08d7b43fe32e/prices""
+        },
+        ""amount"": 0,
+        ""description"": ""Doom & Gloom"",
+        ""initiatingSystemUserAgent"": ""PostmanRuntime/7.22.0"",
+        ""userAgent"": ""Mozilla/5.0 Postman"",
+        ""language"": ""nb-NO"",
+        ""urls"": {
+            ""id"": ""/psp/creditcard/payments/07f14eae-ee4d-414d-2635-08d7b43fe32e/urls""
+        },
+        ""payeeInfo"": {
+            ""id"": ""/psp/creditcard/payments/07f14eae-ee4d-414d-2635-08d7b43fe32e/payeeinfo""
+        },
+        ""metadata"": {
+            ""id"": ""/psp/creditcard/payments/07f14eae-ee4d-414d-2635-08d7b43fe32e/metadata""
+        }
+    },
+    ""operations"": [
+        {
+            ""method"": ""PATCH"",
+            ""href"": ""https://api.stage.payex.com/psp/creditcard/payments/07f14eae-ee4d-414d-2635-08d7b43fe32e"",
+            ""rel"": ""update-payment-abort"",
+            ""contentType"": ""application/json""
+        },
+        {
+            ""method"": ""GET"",
+            ""href"": ""https://ecom.stage.payex.com/creditcardv2/payments/authorize/3b33a04733abc08f9331b4eaff1f8c9af65268b93914e186f8217069fe699d71"",
+            ""rel"": ""redirect-authorization"",
+            ""contentType"": ""text/html""
+        },
+        {
+            ""method"": ""GET"",
+            ""href"": ""https://ecom.stage.payex.com/test"",
+            ""rel"": ""unknown-test-operation"",
+            ""contentType"": ""text/html""
+        }
+    ]
+}";
     }
 }
