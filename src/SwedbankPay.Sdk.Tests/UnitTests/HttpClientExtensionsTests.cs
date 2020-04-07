410900538c478eb4cca5f7321324d9cd1e3ddc2e
Daniel Granerud <daniel.granerud@payex.com>
2020-04-07 11:13:16 +0200

    DX-883 Add a new test for converting

--- src/SwedbankPay.Sdk.Tests/UnitTests/HttpClientExtensionsTests.cs
+++ src/SwedbankPay.Sdk.Tests/UnitTests/HttpClientExtensionsTests.cs
@@ -1,5 +1,7 @@
 ﻿using SwedbankPay.Sdk.Exceptions;
 using SwedbankPay.Sdk.Extensions;
+using SwedbankPay.Sdk.PaymentOrders;
+using SwedbankPay.Sdk.Payments;
 using SwedbankPay.Sdk.Tests.TestBuilders;
 using SwedbankPay.Sdk.Tests.TestHelpers;
 using System;
@@ -24,7 +26,7 @@ namespace SwedbankPay.Sdk.Tests.UnitTests
         }
 
         [Fact]
-        public async Task ApiError_DoesNotResultInWrongException_WhenDataIsSerialized()
+        public async Task ProblemResponse_CorrectlySerializes_WithNoAdditionalErrors()
         {
             var handler = new FakeDelegatingHandler();
             handler.FakeResponseList.Add(new HttpResponseMessage
@@ -48,7 +50,86 @@ namespace SwedbankPay.Sdk.Tests.UnitTests
             var uri = new Uri("http://api.externalintegration.payex.com");
             var sut = new HttpClient(handler);
 
-            var error = await Assert.ThrowsAsync<HttpResponseException>(() => sut.SendAndProcessAsync<object>(HttpMethod.Get, uri, null));
+            var error = await Assert.ThrowsAsync<HttpResponseException>(() => sut.SendAndProcessAsync<ProblemResponse>(HttpMethod.Get, uri, null));
+
+            Assert.Equal(3, error.Data.Count);
+        }
+
+        [Fact]
+        public async Task ProblemResponse_CorrectlySerialzes_WhenApiReturnsWrongStatusCode()
+        {
+            var handler = new FakeDelegatingHandler();
+            handler.FakeResponseList.Add(new HttpResponseMessage
+            {
+                StatusCode = System.Net.HttpStatusCode.OK,
+                Content = new StringContent(@"{
+                    ""sessionId"": ""09ccd29a-7c4f-4752-9396-12100cbfecce"",
+                    ""type"": ""https://api.payex.com/psp/errordetail/inputerror"",
+                    ""title"": ""Error in input data"",
+                    ""status"": 400,
+                    ""instance"": ""http://api.externalintegration.payex.com/psp/09ccd29a-7c4f-4752-9396-12100cbfecce/captures"",
+                    ""detail"": ""Input validation failed, error description in problems node!"",
+                    ""problems"": [
+                        {
+                            ""name"": ""Transaction.Amount"",
+                            ""description"": ""  ""
+                        }
+                    ]
+                }")
+            });
+            var uri = new Uri("http://api.externalintegration.payex.com");
+            var sut = new HttpClient(handler);
+
+            var result = await sut.SendAndProcessAsync<ProblemResponse>(HttpMethod.Get, uri, null);
+
+            Assert.IsType<ProblemResponse>(result);
+        }
+
+        [Fact]
+        public async Task WhenSendingACapture_TheConverter_HasZeroErrors()
+        {
+            var captureRespone = new CaptureResponse
+                (new Uri("/psp/invoice/payments/7482db67-d2d8-4881-79d9-08d7dad1a06b", UriKind.Relative),
+                new TransactionResponse(new Uri("/psp/invoice/payments/7482db67-d2d8-4881-79d9-08d7dad1a06b/captures/d07e17bf-8664-4664-fade-08d7dace174a", UriKind.Relative),
+                new Transaction(new Uri("/psp/invoice/payments/7482db67-d2d8-4881-79d9-08d7dad1a06b/captures/d07e17bf-8664-4664-fade-08d7dace174a", UriKind.Relative),
+                                DateTime.Now,
+                                DateTime.Now,
+                                TransactionType.Capture,
+                                State.Completed,
+                                71100590865,
+                                new Amount(25767),
+                                new Amount(0),
+                                "Description",
+                                "637218522761159010",
+                                false,
+                                new OperationList(),
+                                string.Empty)));
+            var handler = new FakeDelegatingHandler();
+            handler.FakeResponseList.Add(new HttpResponseMessage
+            {
+                StatusCode = System.Net.HttpStatusCode.OK,
+                Content = new StringContent(Newtonsoft.Json.JsonConvert.SerializeObject(captureRespone, JsonSerialization.JsonSerialization.Settings))
+            });
+            var uri = new Uri("http://api.externalintegration.payex.com");
+            
+
+            var captureRequest = new PaymentOrderCaptureRequest(new Amount(25767), new Amount(0), new System.Collections.Generic.List<OrderItem>
+            {
+                new OrderItem(
+                    "Test",
+                    "Test",
+                    OrderItemType.Other,
+                    "Capture",
+                    1,
+                    "pcs",
+                    new Amount(25767),
+                    0,
+                    new Amount(25767),
+                    new Amount(0))
+            }, "Capturing payment.", "637218522761159010");
+
+            var sut = new HttpClient(handler);
+            var error = await Assert.ThrowsAsync<HttpResponseException>(() => sut.SendAndProcessAsync<CaptureResponse>(HttpMethod.Get, uri, captureRequest));
 
             Assert.Equal(3, error.Data.Count);
         }
