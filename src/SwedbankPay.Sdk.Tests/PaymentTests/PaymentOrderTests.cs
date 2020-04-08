b920fc0708fe52dd9f182a002b18a72a9d3cae62
Daniel Granerud <daniel.granerud@payex.com>
2020-04-08 10:25:27 +0200

    DX-883 Cleanup and format test files

--- src/SwedbankPay.Sdk.Tests/PaymentTests/PaymentOrderTests.cs
+++ src/SwedbankPay.Sdk.Tests/PaymentTests/PaymentOrderTests.cs
@@ -4,7 +4,6 @@ using SwedbankPay.Sdk.Tests.TestHelpers;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
--- src/SwedbankPay.Sdk.Tests/UnitTests/HttpClientExtensionsTests.cs
+++ src/SwedbankPay.Sdk.Tests/UnitTests/HttpClientExtensionsTests.cs
@@ -13,6 +13,64 @@ namespace SwedbankPay.Sdk.Tests.UnitTests
 {
     public class HttpClientExtensionsTests : ResourceTestsBase
     {
+        private const string PaymentOrderInputValidationFailedReponse = @"{
+            ""sessionId"": ""09ccd29a-7c4f-4752-9396-12100cbfecce"",
+            ""type"": ""https://api.payex.com/psp/errordetail/inputerror"",
+            ""title"": ""Error in input data"",
+            ""status"": 400,
+            ""instance"": ""http://api.externalintegration.payex.com/psp/09ccd29a-7c4f-4752-9396-12100cbfecce/captures"",
+            ""detail"": ""Input validation failed, error description in problems node!"",
+            ""problems"": [
+                {
+                    ""name"": ""Transaction.Amount"",
+                    ""description"": ""  ""
+                }
+            ]
+        }";
+
+        private const string PaymentCompletedReponse = @"{
+            ""payment"": ""/psp/invoice/payments/09ccd29a-7c4f-4752-9396-12100cbfecce"",
+            ""capture"": {
+                ""itemDescriptions"": {
+                    ""id"": ""/psp/invoice/payments/09ccd29a-7c4f-4752-9396-12100cbfecce/transactions/d07e17bf-8664-4664-fade-08d7dace174a/itemdescriptions""
+                },
+                ""id"": ""/psp/invoice/payments/09ccd29a-7c4f-4752-9396-12100cbfecce/captures/d07e17bf-8664-4664-fade-08d7dace174a"",
+                ""transaction"": {
+                    ""id"": ""/psp/invoice/payments/09ccd29a-7c4f-4752-9396-12100cbfecce/transactions/d07e17bf-8664-4664-fade-08d7dace174a"",
+                    ""created"": ""2020-04-07T08:57:05.7367464Z"",
+                    ""updated"": ""2020-04-07T08:57:07.1111552Z"",
+                    ""type"": ""Capture"",
+                    ""state"": ""Completed"",
+                    ""number"": 71100590865,
+                    ""amount"": 2,
+                    ""vatAmount"": 0,
+                    ""description"": ""description"",
+                    ""payeeReference"": ""637218522761159010"",
+                    ""isOperational"": false,
+                    ""reconciliationNumber"": 0001,
+                    ""operations"": []
+                }
+            }
+        }";
+
+        private static PaymentOrderCaptureRequest GetPaymentOrderCaptureRequest()
+        {
+            return new PaymentOrderCaptureRequest(new Amount(25767), new Amount(0), new System.Collections.Generic.List<OrderItem>
+            {
+                new OrderItem(
+                    "Test",
+                    "Test",
+                    OrderItemType.Other,
+                    "Capture",
+                    1,
+                    "pcs",
+                    new Amount(25767),
+                    0,
+                    new Amount(25767),
+                    new Amount(0))
+            }, "Capturing payment.", "637218522761159010");
+        }
+
         [Fact]
         public async Task ApiError_AreCorrectlySerialized_WithCorrectAmountOfDataAdded()
         {
@@ -32,20 +90,7 @@ namespace SwedbankPay.Sdk.Tests.UnitTests
             handler.FakeResponseList.Add(new HttpResponseMessage
             {
                 StatusCode = System.Net.HttpStatusCode.BadRequest,
-                Content = new StringContent(@"{
-                    ""sessionId"": ""09ccd29a-7c4f-4752-9396-12100cbfecce"",
-                    ""type"": ""https://api.payex.com/psp/errordetail/inputerror"",
-                    ""title"": ""Error in input data"",
-                    ""status"": 400,
-                    ""instance"": ""http://api.externalintegration.payex.com/psp/09ccd29a-7c4f-4752-9396-12100cbfecce/captures"",
-                    ""detail"": ""Input validation failed, error description in problems node!"",
-                    ""problems"": [
-                        {
-                            ""name"": ""Transaction.Amount"",
-                            ""description"": ""  ""
-                        }
-                    ]
-                }")
+                Content = new StringContent(PaymentOrderInputValidationFailedReponse)
             });
             var uri = new Uri("http://api.externalintegration.payex.com");
             var sut = new HttpClient(handler);
@@ -62,20 +107,7 @@ namespace SwedbankPay.Sdk.Tests.UnitTests
             handler.FakeResponseList.Add(new HttpResponseMessage
             {
                 StatusCode = System.Net.HttpStatusCode.OK,
-                Content = new StringContent(@"{
-                    ""sessionId"": ""09ccd29a-7c4f-4752-9396-12100cbfecce"",
-                    ""type"": ""https://api.payex.com/psp/errordetail/inputerror"",
-                    ""title"": ""Error in input data"",
-                    ""status"": 400,
-                    ""instance"": ""http://api.externalintegration.payex.com/psp/09ccd29a-7c4f-4752-9396-12100cbfecce/captures"",
-                    ""detail"": ""Input validation failed, error description in problems node!"",
-                    ""problems"": [
-                        {
-                            ""name"": ""Transaction.Amount"",
-                            ""description"": ""  ""
-                        }
-                    ]
-                }")
+                Content = new StringContent(PaymentOrderInputValidationFailedReponse)
             });
             var uri = new Uri("http://api.externalintegration.payex.com");
             var sut = new HttpClient(handler);
@@ -92,53 +124,12 @@ namespace SwedbankPay.Sdk.Tests.UnitTests
             handler.FakeResponseList.Add(new HttpResponseMessage
             {
                 StatusCode = System.Net.HttpStatusCode.OK,
-                Content = new StringContent(@"{
-    ""payment"": ""/psp/invoice/payments/09ccd29a-7c4f-4752-9396-12100cbfecce"",
-    ""capture"": {
-        ""itemDescriptions"": {
-            ""id"": ""/psp/invoice/payments/09ccd29a-7c4f-4752-9396-12100cbfecce/transactions/d07e17bf-8664-4664-fade-08d7dace174a/itemdescriptions""
-        },
-        ""id"": ""/psp/invoice/payments/09ccd29a-7c4f-4752-9396-12100cbfecce/captures/d07e17bf-8664-4664-fade-08d7dace174a"",
-        ""transaction"": {
-            ""id"": ""/psp/invoice/payments/09ccd29a-7c4f-4752-9396-12100cbfecce/transactions/d07e17bf-8664-4664-fade-08d7dace174a"",
-            ""created"": ""2020-04-07T08:57:05.7367464Z"",
-            ""updated"": ""2020-04-07T08:57:07.1111552Z"",
-            ""type"": ""Capture"",
-            ""state"": ""Completed"",
-            ""number"": 71100590865,
-            ""amount"": 2,
-            ""vatAmount"": 0,
-            ""description"": ""description"",
-            ""payeeReference"": ""637218522761159010"",
-            ""isOperational"": false,
-            ""reconciliationNumber"": 0001,
-            ""operations"": []
-        }
-    }
-}")
+                Content = new StringContent(PaymentCompletedReponse)
             });
             var uri = new Uri("http://api.externalintegration.payex.com");
-            
-
-            var captureRequest = new PaymentOrderCaptureRequest(new Amount(25767), new Amount(0), new System.Collections.Generic.List<OrderItem>
-            {
-                new OrderItem(
-                    "Test",
-                    "Test",
-                    OrderItemType.Other,
-                    "Capture",
-                    1,
-                    "pcs",
-                    new Amount(25767),
-                    0,
-                    new Amount(25767),
-                    new Amount(0))
-            }, "Capturing payment.", "637218522761159010");
 
             var sut = new HttpClient(handler);
-            var error = await Assert.ThrowsAsync<HttpResponseException>(() => sut.SendAndProcessAsync<CaptureResponse>(HttpMethod.Get, uri, captureRequest));
-
-            Assert.Equal(3, error.Data.Count);
+            await sut.SendAndProcessAsync<CaptureResponse>(HttpMethod.Get, uri, GetPaymentOrderCaptureRequest());
         }
 
         [Fact]
@@ -148,20 +139,7 @@ namespace SwedbankPay.Sdk.Tests.UnitTests
             handler.FakeResponseList.Add(new HttpResponseMessage
             {
                 StatusCode = System.Net.HttpStatusCode.BadRequest,
-                Content = new StringContent(@"{
-                    ""sessionId"": ""09ccd29a-7c4f-4752-9396-12100cbfecce"",
-                    ""type"": ""https://api.payex.com/psp/errordetail/inputerror"",
-                    ""title"": ""Error in input data"",
-                    ""status"": 400,
-                    ""instance"": ""http://api.externalintegration.payex.com/psp/09ccd29a-7c4f-4752-9396-12100cbfecce/captures"",
-                    ""detail"": ""Input validation failed, error description in problems node!"",
-                    ""problems"": [
-                        {
-                            ""name"": ""Transaction.Amount"",
-                            ""description"": ""  ""
-                        }
-                    ]
-                }")
+                Content = new StringContent(PaymentOrderInputValidationFailedReponse)
             });
             var uri = new Uri("http://api.externalintegration.payex.com");
             var sut = new HttpClient(handler);
