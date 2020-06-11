7450f9f3a49d64cba61b1e841a4ad9993149bd18
Daniel Granerud <daniel.granerud@payex.com>
2020-06-11 12:29:44 +0200

    DX-952 Run our own nuget push script

---
+++ .github/workflows/nuget_push.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+help_message="\
+Usage:
+
+Runs 'dotnet package' and 'dotnet nuget push'.
+
+VERSION_NUMBER: The version number for the nuget.
+  PROJECT_FILE: The project file for the nuget.
+     NUGET_KEY: The Key to the Nugets. It's in the name."
+
+if [[ -z "$VERSION_NUMBER" ]]; then
+    echo "Missing or empty VERSION_NUMBER environment variable." >&2
+    echo "$help_message"
+    exit 1
+fi
+
+if [[ -z "$PROJECT_FILE" ]]; then
+    echo "Missing or empty PROJECT_FILE environment variable." >&2
+    echo "$help_message"
+    exit 1
+fi
+if [[ -z "$NUGET_KEY" ]]; then
+    echo "Missing or empty NUGET_KEY environment variable." >&2
+    echo "$help_message"
+    exit 1
+fi
+
+dotnet pack -p:PackageVersion="$VERSION_NUMBER"  -c Release "$PROJECT_FILE" -o nugets/
+dotnet nuget push nugets/*.nupkg -s https://api.nuget.org/v3/index.json -k "$NUGET_KEY" --skip-duplicate
