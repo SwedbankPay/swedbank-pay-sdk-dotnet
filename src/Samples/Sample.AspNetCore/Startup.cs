b6c906414b8f3bebaf9f65e6445c1b3936448dc1
Daniel Granerud <daniel.granerud@payex.com>
2020-06-19 14:31:19 +0200

    DX-952 Add Serilog to log to file

--- src/Samples/Sample.AspNetCore/Startup.cs
+++ src/Samples/Sample.AspNetCore/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Sample.AspNetCore.Data;
 using Sample.AspNetCore.Extensions;
 using Sample.AspNetCore.Models;
@@ -24,7 +25,7 @@ namespace Sample.AspNetCore
 
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
@@ -50,6 +51,8 @@ namespace Sample.AspNetCore
                     "default",
                     "{controller=Products}/{action=Index}/{id?}");
             });
+
+            loggerFactory.AddFile("Logs/Sample.log");
         }
 
 
