328d71f6541cf0f48ce0160c51bce780ad6156cd
Daniel Granerud <daniel.granerud@payex.com>
2020-06-25 09:49:30 +0200

    Add a $

--- .github/workflows/nuget_push.sh
+++ .github/workflows/nuget_push.sh
@@ -30,7 +30,7 @@ if [[ -z "$NUGET_KEY" ]]; then
     exit 1
 fi
 
-sanitized_version_number=${VERSION_NUMBER//\+/.}
+sanitized_version_number=${$VERSION_NUMBER//\+/.}
 
 dotnet pack -p:PackageVersion="$sanitized_version_number"  -c Release "$PROJECT_FILE" -o nugets/
 dotnet nuget push nugets/*.nupkg -s https://api.nuget.org/v3/index.json -k "$NUGET_KEY" --skip-duplicate
