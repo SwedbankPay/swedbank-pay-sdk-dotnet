24a646a2898a35c33062a2754e362ceb92fa3eee
Daniel Sundqvist <daniel.sundqvist@authority.se>
2020-07-09 13:47:47 +0200

    Removed Sale operation

--- src/SwedbankPay.Sdk/Payments/TrustlyPayments/TrustlyPaymentOperations.cs
+++ src/SwedbankPay.Sdk/Payments/TrustlyPayments/TrustlyPaymentOperations.cs
@@ -4,7 +4,6 @@ using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 
-
 namespace SwedbankPay.Sdk.Payments.TrustlyPayments
 {
     public class TrustlyPaymentOperations : OperationsBase
@@ -15,6 +14,14 @@ namespace SwedbankPay.Sdk.Payments.TrustlyPayments
             {
                 switch (httpOperation.Rel.Value)
                 {
+                    case PaymentResourceOperations.RedirectSale:
+                        RedirectSale = httpOperation;
+                        break;
+
+                    case PaymentResourceOperations.ViewSales:
+                        ViewSale = httpOperation;
+                        break;
+
                     case PaymentResourceOperations.UpdatePaymentAbort:
                         Abort = async payload =>
                             await client.SendAsJsonAsync<TrustlyPaymentResponse>(httpOperation.Method, httpOperation.Href, payload);
@@ -23,19 +30,6 @@ namespace SwedbankPay.Sdk.Payments.TrustlyPayments
                     case PaymentResourceOperations.CreateReversal:
                         Reverse = async payload =>
                             await client.SendAsJsonAsync<ReversalResponse>(httpOperation.Method, httpOperation.Href, payload);
-                        break;
-
-                    case PaymentResourceOperations.CreateSale:
-                        CreateSale = async payload =>
-                            await client.SendAsJsonAsync<TrustlyPaymentSaleResponse>(httpOperation.Method, httpOperation.Href, payload);
-                        break;
-
-                    case PaymentResourceOperations.RedirectSale:
-                        RedirectSale = httpOperation;
-                        break;
-
-                    case PaymentResourceOperations.ViewSales:
-                        ViewSale = httpOperation;
                         break;
 
                     case PaymentResourceOperations.CreateCancellation:
@@ -47,14 +41,9 @@ namespace SwedbankPay.Sdk.Payments.TrustlyPayments
             }
         }
 
-        public Func<PaymentAbortRequest, Task<TrustlyPaymentResponse>> Abort { get; }
-
-        public Func<TrustlyPaymentSaleRequest, Task<TrustlyPaymentSaleResponse>> CreateSale { get; }
-        
         public HttpOperation RedirectSale { get; }
-
         public HttpOperation ViewSale { get; }
-
+        public Func<PaymentAbortRequest, Task<TrustlyPaymentResponse>> Abort { get; }
         public Func<TrustlyPaymentReversalRequest, Task<ReversalResponse>> Reverse { get; }
         public Func<TrustlyPaymentCancelRequest, Task<CancellationResponse>> Cancel { get; }
     }
