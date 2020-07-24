1113e37ff9b346327fd1f0c3d60d4bb3e5a68186
trieutran91 <trieu.tran@payex.com>
2020-07-24 11:36:58 +0200

    dx-1114 Removing properties in Verify request

--- src/SwedbankPay.Sdk/Payments/CardPayments/CardPaymentVerifyRequest.cs
+++ src/SwedbankPay.Sdk/Payments/CardPayments/CardPaymentVerifyRequest.cs
@@ -1,10 +1,72 @@
-﻿using System;
+﻿using SwedbankPay.Sdk.PaymentOrders;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Globalization;
 
 namespace SwedbankPay.Sdk.Payments.CardPayments
 {
-    class CardPaymentVerifyRequest
+    public class CardPaymentVerifyRequest
     {
+        public CardPaymentVerifyRequest(Operation operation,
+                              CurrencyCode currency,
+                              string description,
+                              string userAgent,
+                              CultureInfo language,
+                              Urls urls,
+                              PayeeInfo payeeInfo,
+                              string payerReference = null,
+                              bool generatePaymentToken = false,
+                              bool generateReccurenceToken = false,
+                              CreditCard creditCard = null)
+        {
+            this.Verify = new CardPaymentVerifyRequestObject(operation, currency, description, payerReference, generatePaymentToken,
+                                               generateReccurenceToken, userAgent, language, urls, payeeInfo,
+                                               creditCard);
+        }
+
+
+        public CardPaymentVerifyRequestObject Verify;
+
+        public class CardPaymentVerifyRequestObject
+        {
+            protected internal CardPaymentVerifyRequestObject(Operation operation,
+                                                    CurrencyCode currency,
+                                                    string description,
+                                                    string payerReference,
+                                                    bool generatePaymentToken,
+                                                    bool generateReccurenceToken,
+                                                    string userAgent,
+                                                    CultureInfo language,
+                                                    Urls urls,
+                                                    PayeeInfo payeeInfo,
+                                                    CreditCard creditCard = null)
+            {
+                Operation = operation ?? throw new ArgumentNullException(nameof(operation));
+                Currency = currency;
+                Description = description;
+                PayerReference = payerReference;
+                UserAgent = userAgent;
+                Language = language;
+                Urls = urls;
+                PayeeInfo = payeeInfo;
+                CreditCard = creditCard;
+                GeneratePaymentToken = generatePaymentToken;
+                GenerateReccurenceToken = generateReccurenceToken;
+            }
+
+
+            public CreditCard CreditCard;
+            public CurrencyCode Currency;
+            public string Description;
+            public bool GeneratePaymentToken;
+            public bool GenerateReccurenceToken;
+            public CultureInfo Language;
+            public Operation Operation;
+            public PayeeInfo PayeeInfo;
+            public string PayerReference;
+            public Urls Urls;
+            public string UserAgent;
+        }
     }
 }
+
