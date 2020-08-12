239f68f9ac639df8c56eaefd46aefa9b03a7364e
Daniel Granerud <daniel.granerud@payex.com>
2020-08-12 10:00:00 +0200

    Fix test in ValidationTests

--- src/Samples/Sample.AspNetCore.SystemTests/Test/PaymentTests/Validation/ValidationTests.cs
+++ src/Samples/Sample.AspNetCore.SystemTests/Test/PaymentTests/Validation/ValidationTests.cs
@@ -57,6 +57,7 @@ namespace Sample.AspNetCore.SystemTests.Test.PaymentTests.Validation
                 .ValidationIcons[x => x.Email].Should.BeVisible()
                 .ValidationIcons[x => x.PhoneNumber].Should.BeVisible()
                 .ValidationIcons[x => x.ZipCode].Should.BeVisible()
+                .PersonalNumber.Clear()
                 .PersonalNumber.SetWithSpeed(TestDataService.PersonalNumberShort, 0.15)
                 .Email.Set(TestDataService.Email)
                 .PhoneNumber.Set(TestDataService.PhoneNumber)
