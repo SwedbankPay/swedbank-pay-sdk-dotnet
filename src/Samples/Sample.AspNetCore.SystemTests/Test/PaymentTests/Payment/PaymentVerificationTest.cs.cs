b15138cc4e7fca05f7d5a999b2e31e6b9fc3a70b
trieutran91 <trieu.tran@payex.com>
2020-09-18 09:40:50 +0200

    dx-974 removed Payex.Psp.Logging.Sla as submodule

---
+++ src/Samples/Sample.AspNetCore.SystemTests/Test/PaymentTests/Payment/PaymentVerificationTest.cs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Atata;
+using NUnit.Framework;
+using Sample.AspNetCore.SystemTests.Test.Helpers;
+
+
+namespace Sample.AspNetCore.SystemTests.Test.PaymentTests.Payment
+{
+    class PaymentVerificationTest: Base.PaymentTests
+    {
+        public PaymentVerificationTest(string driverAlias)
+            : base(driverAlias)
+        {
+        }
+
+        [Test]
+        [Retry(3)]
+        [TestCaseSource(nameof(TestData), new object[] { false, PaymentMethods.Card })]
+        public void Payment_Card_Verify(Product[] products, PayexInfo payexInfo)
+        {
+            GoToVerifyPage();
+            var verifyPage = GoToVerifyPage();
+            verifyPage.VerifyButton.ClickAndGo();
+            var paymentPage = verifyPage.VerifyButton.ClickAndGo();
+            paymentPage.FillInCreditCardInfo();
+
+        }
+
+    }
+}
