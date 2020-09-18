9244afe6017b0769c1495d182182e929ab543e69
trieutran91 <trieu.tran@payex.com>
2020-09-18 13:46:56 +0200

    dx-974 Added redirect to error page and error message in Verify controller

--- src/Samples/Sample.AspNetCore/Controllers/VerifyController.cs
+++ src/Samples/Sample.AspNetCore/Controllers/VerifyController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Globalization;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
@@ -18,10 +19,6 @@ namespace Sample.AspNetCore.Controllers
         private readonly Cart cartService;
         private readonly PayeeInfoConfig payeeInfoOptions;
         private readonly UrlsOptions urls;
-        private bool generatePaymentToken;
-        private bool generateReccurenceToken;
-        private string description;
-        private string userAgent;
         private readonly StoreDbContext context;
         private readonly SwedbankPayConnectionSettings swedbankPayConnectionSettings;
 
@@ -43,17 +40,17 @@ namespace Sample.AspNetCore.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Error(string message)
         {
+            ViewBag.Message = message;
             return View();
         }
 
-        public async Task<CardVerify> InitiateVerify()
-        {
-            try
+        private async Task<CardVerify> InitiateVerify()
+       
             {
                 var verifyRequest = new CardPaymentVerifyRequest(Operation.Verify, new CurrencyCode("SEK"),
-                                                                                   description = "Test Verification", userAgent = "useragent",
+                                                                                   "Test Verification", "useragent",
                                                                                    CultureInfo.GetCultureInfo("sv-SE"),
                                                                                    new Urls(this.urls.HostUrls, this.urls.VerificationListUrl,
                                                                                             this.urls.TermsOfServiceUrl, this.urls.CancelUrl,
@@ -61,8 +58,8 @@ namespace Sample.AspNetCore.Controllers
                                                                                    new PayeeInfo(this.payeeInfoOptions.PayeeId,
                                                                                                  this.payeeInfoOptions.PayeeReference),
                                                                                    this.payeeInfoOptions.PayeeReference,
-                                                                                   generatePaymentToken = true,
-                                                                                   generateReccurenceToken = true)
+                                                                                   true,
+                                                                                   true)
                                                                                    ;
 
                 var cardVerify = await this.swedbankPayClient.Payments.CardPayments.Verify(verifyRequest);
@@ -71,19 +68,21 @@ namespace Sample.AspNetCore.Controllers
 
                 return cardVerify;
             }
-            catch (Exception ex)
-            {
-                Debug.Write(ex.Message);
-                return null;
-            }
-        }
 
         [HttpGet]
         public async Task<IActionResult> GetRedirectVerificationHref(string consumerProfileRef = null)
         {
+            try
+            {
             var verify = await InitiateVerify();
-
+    
             return new RedirectResult(verify.Operations.RedirectVerification.Href.ToString());
+
+            }
+            catch(Exception ex)
+            {
+                return RedirectToAction("Error", ex.Message);
+            }
         }
 
         [HttpGet]
