833b5f23a6580f2799ec22fc96ab099a07ba8535
Daniel Granerud <daniel.granerud@payex.com>
2020-10-06 11:21:15 +0200

    Add some missing usings after moving files

--- src/SwedbankPay.Sdk.Infrastructure/Payments/CardPaymentsResource.cs
+++ src/SwedbankPay.Sdk.Infrastructure/Payments/CardPaymentsResource.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SwedbankPay.Sdk.Payments.CardPayments;
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 
--- src/SwedbankPay.Sdk.Infrastructure/Payments/VippsPaymentsResource.cs
+++ src/SwedbankPay.Sdk.Infrastructure/Payments/VippsPaymentsResource.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SwedbankPay.Sdk.Payments.VippsPayments;
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 
