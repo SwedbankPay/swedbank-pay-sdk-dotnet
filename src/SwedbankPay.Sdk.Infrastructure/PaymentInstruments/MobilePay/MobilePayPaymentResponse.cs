774fb773dae6aee806c80192150798446c59858b
Daniel Granerud <daniel.granerud@payex.com>
2020-10-14 15:38:36 +0200

    Update MobilePayPaymentResponse interface and class

--- src/SwedbankPay.Sdk.Infrastructure/PaymentInstruments/MobilePay/MobilePayPaymentResponse.cs
+++ src/SwedbankPay.Sdk.Infrastructure/PaymentInstruments/MobilePay/MobilePayPaymentResponse.cs
@@ -1,4 +1,7 @@
-﻿using System.Net.Http;
+﻿using SwedbankPay.Sdk.PaymentOrders;
+using System;
+using System.Globalization;
+using System.Net.Http;
 
 namespace SwedbankPay.Sdk.Payments.MobilePayPayments
 {
@@ -10,7 +13,29 @@ namespace SwedbankPay.Sdk.Payments.MobilePayPayments
             //ToDO: Fill out
         }
 
-        public IMobilePayPaymentResponse Payment { get; }
-        public IOperationList Operations { get; }
+        public Amount Amount { get; }
+        public IMobilePayPaymentAuthorizationListResponse Authorizations { get; }
+        public ICancellationsListResponse Cancellations { get; }
+        public ICapturesListResponse Captures { get; }
+        public DateTime Created { get; }
+        public DateTime Updated { get; }
+        public CurrencyCode Currency { get; }
+        public string Description { get; }
+        public Uri Id { get; }
+        public PaymentInstrument Instrument { get; }
+        public PaymentIntent Intent { get; }
+        public CultureInfo Language { get; }
+        public string Number { get; }
+        public Operation Operation { get; }
+        public PayeeInfo PayeeInfo { get; }
+        public string PayerReference { get; }
+        public string InitiatingSystemUserAgent { get; }
+        public IPricesListResponse Prices { get; }
+        public IReversalsListResponse Reversals { get; }
+        public State State { get; }
+        public ITransactionListResponse Transactions { get; }
+        public IUrls Urls { get; }
+        public string UserAgent { get; }
+        public MetadataResponse Metadata { get; }
     }
 }
