33710a29cbae87d26257081e7a6202b84566f6fc
Daniel Granerud <daniel.granerud@payex.com>
2020-10-22 15:49:44 +0200

    Move CancellationList response dto

---
+++ src/SwedbankPay.Sdk.Infrastructure/PaymentInstruments/CancellationsListResponseDto.cs
@@ -0,0 +1,22 @@
+﻿using SwedbankPay.Sdk.PaymentInstruments.Card;
+using System;
+using System.Collections.Generic;
+
+namespace SwedbankPay.Sdk.PaymentInstruments
+{
+    public class CancellationsListResponseDto
+    {
+        public List<TransactionDto> CancellationList { get; set; }
+        public Uri Id { get; set; }
+
+        internal ICancellationsListResponse Map()
+        {
+            var transactionList = new List<ITransactionResponse>();
+            foreach (var t in CancellationList)
+            {
+                transactionList.Add(new TransactionResponse(Id, t));
+            }
+            return new CancellationsListResponse(Id, transactionList);
+        }
+    }
+}
