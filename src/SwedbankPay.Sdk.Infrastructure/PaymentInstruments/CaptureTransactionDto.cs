49b11e8c7f8b9fadae4ac1e468c9e9bdf6aee388
Daniel Granerud <daniel.granerud@payex.com>
2020-10-23 12:16:41 +0200

    Update CaptureTransactioDto

--- src/SwedbankPay.Sdk.Infrastructure/PaymentInstruments/CaptureTransactionDto.cs
+++ src/SwedbankPay.Sdk.Infrastructure/PaymentInstruments/CaptureTransactionDto.cs
@@ -1,4 +1,5 @@
-﻿using SwedbankPay.Sdk.PaymentInstruments.Card;
+﻿using SwedbankPay.Sdk.Common;
+using SwedbankPay.Sdk.PaymentInstruments.Card;
 using SwedbankPay.Sdk.PaymentOrders;
 using System;
 using System.Collections.Generic;
@@ -7,16 +8,16 @@ namespace SwedbankPay.Sdk.PaymentInstruments
 {
     public class CaptureTransactionDto : List<PaymentOrderTransactionDto>
     {
-        public string Id { get; set; }
+        public Uri Id { get; set; }
 
         internal ICapturesListResponse Map()
         {
             var list = new List<ITransactionResponse>();
             foreach (var item in this)
             {
-                list.Add(new TransactionResponse(item.Id, item));
+                list.Add(new TransactionResponse(Id?.OriginalString, item));
             }
-            return new CapturesListResponse(new Uri(Id, UriKind.RelativeOrAbsolute), list);
+            return new CapturesListResponse(Id, list);
         }
     }
 }
