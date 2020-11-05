a678ddcb057b522191c5a4027145d50160394a01
Daniel Granerud <daniel.granerud@payex.com>
2020-11-05 08:45:02 +0100

    Delte commented out tests

--- src/SwedbankPay.Sdk.Tests/PaymentTests/CardPaymentTests.cs
+++ src/SwedbankPay.Sdk.Tests/PaymentTests/CardPaymentTests.cs
@@ -21,39 +21,6 @@ namespace SwedbankPay.Sdk.Tests.PaymentTests
         }
 
 
-        //[Fact]
-        //public async Task CapturePayment()
-        //{
-        //    var creditCardPayment = await this.Sut.Payment.GetCreditCardPayment(new Uri("/psp/creditcard/payments/16a4cb4f-8d6e-4376-5bf7-08d7942d6bba", UriKind.Relative), PaymentExpand.All);
-        //    var responseContainer = await creditCardPayment.Operations.Capture.Execute(new TransactionRequestContainer<TransactionRequest>(
-        //                                                     new TransactionRequest(Amount.FromDecimal(1600), "description", null,
-        //                                                                            DateTime.Now.Ticks.ToString(), Amount.FromDecimal(0))));
-
-        //    Assert.NotNull(responseContainer);
-        //}
-
-        //[Fact]
-        //public async Task CancelPayment()
-        //{
-        //    var creditCardPayment = await this.Sut.Payment.GetCreditCardPayment(new Uri("/psp/creditcard/payments/23fb6fbd-3f09-4dcc-5d6e-08d7942d6bba", UriKind.Relative), PaymentExpand.All);
-        //    var responseContainer = await creditCardPayment.Operations.Cancel.Execute(new TransactionRequestContainer<CancelTransactionRequest>(new CancelTransactionRequest("description", DateTime.Now.Ticks.ToString())));
-
-        //    Assert.NotNull(responseContainer);
-        //}
-
-        //[Fact]
-        //public async Task ReversalPayment()
-        //{
-        //    var creditCardPayment = await this.Sut.Payment.GetCreditCardPayment(new Uri("/psp/creditcard/payments/16a4cb4f-8d6e-4376-5bf7-08d7942d6bba", UriKind.Relative), PaymentExpand.All);
-
-        //    var responseContainer = await creditCardPayment.Operations.Reversal.Execute(new TransactionRequestContainer<ReversalTransactionRequest>(
-        //                                                                                                       new ReversalTransactionRequest(Amount.FromDecimal(1600), Amount.FromDecimal(0), "description", DateTime.Now.Ticks.ToString())));
-
-        //    Assert.NotNull(responseContainer);
-        //}
-
-
-
         [Fact]
         public async Task CreateVerifyPayment_ShouldReturnPayment()
         {
