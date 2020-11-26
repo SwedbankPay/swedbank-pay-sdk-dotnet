2d91c1184bdb3c103135f9d0047d83af26f58a71
Daniel Granerud <daniel.granerud@payex.com>
2020-11-26 10:25:28 +0100

    Moved all integration tests out of unit test project

---
+++ src/SwedbankPay.Sdk.IntegrationTests/PaymentTests/MobilePayTests.cs
@@ -0,0 +1,21 @@
+﻿using SwedbankPay.Sdk.PaymentInstruments;
+using SwedbankPay.Sdk.Tests.TestBuilders;
+using System.Threading.Tasks;
+using Xunit;
+
+
+namespace SwedbankPay.Sdk.Tests.PaymentTests
+{
+    public class MobilePayTests : ResourceTestsBase
+    {
+        private readonly PaymentRequestBuilder paymentRequestBuilder = new PaymentRequestBuilder();
+
+        [Fact(Skip = "Not supported by merchants that have both Checkout and Payment Pages")]
+        public async Task CreateMobilePayPayment_ShouldReturnPayment()
+        {
+            var paymentRequest = this.paymentRequestBuilder.WithMobilePayTestValues(this.payeeId).BuildMobilePayRequest();
+            var mobilePayPayment = await this.Sut.Payments.MobilePayPayments.Create(paymentRequest, PaymentExpand.All);
+            Assert.NotNull(mobilePayPayment);
+        }
+    }
+}
