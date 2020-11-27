36637c2a845d8b0d9632a42a4d80d6b702c1a827
Daniel Granerud <daniel.granerud@payex.com>
2020-11-27 11:46:49 +0100

    Fix setter for PaymentAbortRequestDetails

--- src/SwedbankPay.Sdk/PaymentInstruments/PaymentAbortRequest.cs
+++ src/SwedbankPay.Sdk/PaymentInstruments/PaymentAbortRequest.cs
@@ -2,9 +2,14 @@
 {
     public class PaymentAbortRequest
     {
+        public PaymentAbortRequest(PaymentAbortRequestDetails payment)
+        {
+            Payment = payment;
+        }
+
         /// <summary>
         /// Data detailing why the current payment is being aborted.
         /// </summary>
-        public PaymentAbortRequestDetails Payment { get; set; } = new PaymentAbortRequestDetails();
+        public PaymentAbortRequestDetails Payment { get; } = new PaymentAbortRequestDetails();
     }
 }
