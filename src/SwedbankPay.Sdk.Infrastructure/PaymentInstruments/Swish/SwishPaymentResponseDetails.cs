5c026390804880fffa51b5625b404b904bcd940b
Daniel Granerud <daniel.granerud@payex.com>
2020-11-27 11:41:08 +0100

    Rename SwishPaymentResponseDetails

---
+++ src/SwedbankPay.Sdk.Infrastructure/PaymentInstruments/Swish/SwishPaymentResponseDetails.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Globalization;
+
+namespace SwedbankPay.Sdk.PaymentInstruments.Swish
+{
+    public class SwishPaymentResponseDetails
+    {
+        public SwishPaymentResponseDetails(Uri id,
+                               long number,
+                               DateTime created,
+                               DateTime updated,
+                               PaymentInstrument instrument,
+                               Operation operation,
+                               PaymentIntent intent,
+                               State state,
+                               Currency currency,
+                               IPricesListResponse prices,
+                               Amount amount,
+                               Amount remainingReversalAmount,
+                               string description,
+                               string payerReference,
+                               string initiatingSystemUserAgent,
+                               string userAgent,
+                               CultureInfo language,
+                               ITransactionListResponse transactions,
+                               ISwishSaleListResponse sales,
+                               IReversalsListResponse reversals,
+                               IUrls urls,
+                               PayeeInfo payeeInfo,
+                               MetadataResponse metadata)
+        {
+            Id = id;
+            Number = number;
+            Created = created;
+            Updated = updated;
+            Instrument = instrument;
+            Operation = operation;
+            Intent = intent;
+            State = state;
+            Currency = currency;
+            Prices = prices;
+            Amount = amount;
+            RemainingReversalAmount = remainingReversalAmount;
+            Description = description;
+            PayerReference = payerReference;
+            InitiatingSystemUserAgent = initiatingSystemUserAgent;
+            UserAgent = userAgent;
+            Language = language;
+            Transactions = transactions;
+            Sales = sales;
+            Reversals = reversals;
+            Urls = urls;
+            PayeeInfo = payeeInfo;
+            Metadata = metadata;
+        }
+
+
+        public long Number { get; }
+        public DateTime Created { get; }
+        public DateTime Updated { get; }
+        public PaymentInstrument Instrument { get; }
+        public Operation Operation { get; }
+        public PaymentIntent Intent { get; }
+        public State State { get; }
+        public Currency Currency { get; }
+        public IPricesListResponse Prices { get; }
+        public Amount Amount { get; }
+        public Amount RemainingReversalAmount { get; set; }
+        public string Description { get; }
+        public string PayerReference { get; }
+        public string InitiatingSystemUserAgent { get; }
+        public string UserAgent { get; }
+        public CultureInfo Language { get; }
+        public ITransactionListResponse Transactions { get; }
+        public ISwishSaleListResponse Sales { get; }
+        public IReversalsListResponse Reversals { get; }
+        public IUrls Urls { get; }
+        public PayeeInfo PayeeInfo { get; }
+        public Uri Id { get; }
+        public MetadataResponse Metadata { get; }
+    }
+}
