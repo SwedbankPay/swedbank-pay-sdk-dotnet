914ced1ed85ef100755e93a94834de9e88d3dac5
Daniel Granerud <daniel.granerud@payex.com>
2020-12-07 12:49:34 +0100

    Remove custom items from json serialization

--- src/SwedbankPay.Sdk.Infrastructure/JsonSerialization/JsonSerialization.cs
+++ src/SwedbankPay.Sdk.Infrastructure/JsonSerialization/JsonSerialization.cs
@@ -19,39 +19,6 @@ namespace SwedbankPay.Sdk.JsonSerialization
                 MaxDepth = 64,
                 IgnoreReadOnlyProperties = false
             };
-
-            Settings.Converters.Add(new CustomAmountConverter());
-            Settings.Converters.Add(new CustomCurrencyCodeConverter());
-            Settings.Converters.Add(new CustomEmailAddressConverter());
-            Settings.Converters.Add(new CustomLanguageConverter());
-            Settings.Converters.Add(new CustomMsisdnConverter());
-            Settings.Converters.Add(new CustomRegionInfoConverter());
-            Settings.Converters.Add(new CustomUriConverter());
-            Settings.Converters.Add(new CustomMetaDataConverter());
-            Settings.Converters.Add(new CustomHttpResponseExceptionConverter());
-
-            // Enum converters
-            Settings.Converters.Add(new TypedSafeEnumValueConverter<ShipIndicator>());
-            Settings.Converters.Add(new TypedSafeEnumValueConverter<DeliveryTimeFrameIndicator>());
-            Settings.Converters.Add(new TypedSafeEnumValueConverter<PreOrderPurchaseIndicator>());
-            Settings.Converters.Add(new TypedSafeEnumValueConverter<ReOrderPurchaseIndicator>());
-            Settings.Converters.Add(new TypedSafeEnumValueConverter<AccountAgeIndicator>());
-            Settings.Converters.Add(new TypedSafeEnumValueConverter<AccountChangeIndicator>());
-            Settings.Converters.Add(new TypedSafeEnumValueConverter<AccountPwdChangeIndicator>());
-            Settings.Converters.Add(new TypedSafeEnumValueConverter<ShippingAddressUsageIndicator>());
-            Settings.Converters.Add(new TypedSafeEnumValueConverter<ShippingNameIndicator>());
-            Settings.Converters.Add(new TypedSafeEnumValueConverter<SuspiciousAccountActivity>());
-            Settings.Converters.Add(new TypedSafeEnumValueConverter<Operation>());
-            Settings.Converters.Add(new TypedSafeEnumValueConverter<State>());
-            Settings.Converters.Add(new TypedSafeEnumValueConverter<PaymentOrderLanguage>());
-            Settings.Converters.Add(new TypedSafeEnumValueConverter<OrderItemType>());
-
-            // Add all normal enums, to support the "Uknown" value.
-            Settings.Converters.Add(new TypesafeEnumConverter<PaymentInstrument>());
-            Settings.Converters.Add(new TypesafeEnumConverter<PaymentIntent>());
-            Settings.Converters.Add(new TypesafeEnumConverter<PriceType>());
-            Settings.Converters.Add(new TypesafeEnumConverter<InvoiceType>());
-            Settings.Converters.Add(new TypesafeEnumConverter<TransactionType>());
         }
 
         public static JsonSerializerOptions Settings { get; private set; }
