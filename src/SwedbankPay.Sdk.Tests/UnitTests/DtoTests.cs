d5637f6adfbb2f35eaaaedbe4f8a6c221aa143a2
nullableint <daniel.granerud@payex.com>
2020-12-08 13:34:52 +0100

    Add tests to verify abort reason string

---
+++ src/SwedbankPay.Sdk.Tests/UnitTests/DtoTests.cs
@@ -0,0 +1,26 @@
+﻿using SwedbankPay.Sdk.PaymentOrders;
+using System;
+using Xunit;
+
+namespace SwedbankPay.Sdk.Tests.UnitTests
+{
+    public class DtoTests
+    {
+        [Theory]
+        [InlineData("te st")]
+        [InlineData("sets data")]
+        [InlineData("test-data")]
+        public void Creating_Abort_WithNotSupportedCharacters_ThrowsException(string data)
+        {
+            Assert.Throws<ArgumentException>(() => new PaymentOrderAbortRequest(data));
+        }
+
+        [Theory]
+        [InlineData("testAbortReason")]
+        [InlineData("CancelledByConsumer")]
+        public void Creating_Abort_WithSupportedCharacters_DoesNotThrowException(string data)
+        {
+            new PaymentOrderAbortRequest(data);
+        }
+    }
+}
