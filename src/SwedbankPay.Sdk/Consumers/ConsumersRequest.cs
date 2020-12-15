64313e085d4384070a6b41b5332c49dffbefc4df
Daniel Granerud <daniel.granerud@payex.com>
2020-12-15 08:06:21 +0100

    Remove optional parameters for Consumers domain

--- src/SwedbankPay.Sdk/Consumers/ConsumersRequest.cs
+++ src/SwedbankPay.Sdk/Consumers/ConsumersRequest.cs
@@ -14,22 +14,11 @@ namespace SwedbankPay.Sdk.Consumers
         /// </summary>
         /// <param name="language">The prefered langauge for the consumer.</param>
         /// <param name="shippingAddressRestrictedToCountryCodes">A <seealso cref="IEnumerable{RegionInfo}"/> that lists available countrys this payment will ship to.</param>
-        /// <param name="operation">The initiating operation for this request.</param>
-        /// <param name="msisdn">The consumers <seealso cref="Sdk.Msisdn"/>.</param>
-        /// <param name="email">The consumers <seealso cref="Sdk.EmailAddress"/>.</param>
-        /// <param name="nationalIdentifier">The consumers <seealso cref="Sdk.PaymentOrders.NationalIdentifier"/>.</param>
         public ConsumersRequest(Language language,
-                                IEnumerable<RegionInfo> shippingAddressRestrictedToCountryCodes,
-                                Operation operation = null,
-                                Msisdn msisdn = null,
-                                EmailAddress email = null,
-                                NationalIdentifier nationalIdentifier = null)
+                                IEnumerable<RegionInfo> shippingAddressRestrictedToCountryCodes)
         {
-            Operation = operation ?? Operation.Initiate;
+            Operation = Operation.Initiate;
             Language = language;
-            Msisdn = msisdn;
-            Email = email;
-            NationalIdentifier = nationalIdentifier;
             ShippingAddressRestrictedToCountryCodes = shippingAddressRestrictedToCountryCodes ?? new List<RegionInfo>();
         }
 
@@ -37,7 +26,7 @@ namespace SwedbankPay.Sdk.Consumers
         /// <summary>
         ///     The e-mail address of the payer.
         /// </summary>
-        public EmailAddress Email { get; }
+        public EmailAddress Email { get; set; }
 
         /// <summary>
         ///     Selected language to be used in Checkin. Supported values are nb-NO, sv-SE and en-US
@@ -47,17 +36,17 @@ namespace SwedbankPay.Sdk.Consumers
         /// <summary>
         ///     The MSISDN (mobile phone number) of the payer. Format Sweden: +46707777777. Format Norway: +4799999999.
         /// </summary>
-        public Msisdn Msisdn { get; }
+        public Msisdn Msisdn { get; set; }
 
         /// <summary>
         /// The payers <seealso cref="Sdk.PaymentOrders.NationalIdentifier"/>.
         /// </summary>
-        public NationalIdentifier NationalIdentifier { get; }
+        public NationalIdentifier NationalIdentifier { get; set; }
 
         /// <summary>
         ///     the operation to perform.
         /// </summary>
-        public Operation Operation { get; }
+        public Operation Operation { get; set; }
 
         /// <summary>
         ///     List of supported shipping countries for merchant. Using ISO-3166 standard.
