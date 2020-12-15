475e413f60cc7660ffa46b78309023c1ef5ebac1
Daniel Granerud <daniel.granerud@payex.com>
2020-12-15 15:26:52 +0100

    Started work on PaymentInstrumentBase

---
+++ src/SwedbankPay.Sdk.Infrastructure/PaymentInstruments/IPaymentInstrumentBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SwedbankPay.Sdk.PaymentInstruments
+{
+    internal interface IPaymentInstrumentBase
+    {
+        Amount Amount { get; }
+        ICancellationsListResponse Cancellations { get; }
+        ICapturesListResponse Captures { get; }
+        DateTime Created { get; }
+        Currency Currency { get; }
+        string Description { get; }
+        Uri Id { get; }
+        string InitiatingSystemUserAgent { get; }
+        PaymentInstrument Instrument { get; }
+        PaymentIntent Intent { get; }
+        Language Language { get; }
+        MetadataResponse Metadata { get; }
+        long Number { get; }
+        Operation Operation { get; }
+        PayeeInfo PayeeInfo { get; }
+        string PayerReference { get; }
+        IPricesListResponse Prices { get; }
+        Amount RemainingCancellationAmount { get; }
+        Amount RemainingCaptureAmount { get; }
+        Amount RemainingReversalAmount { get; }
+        IReversalsListResponse Reversals { get; }
+        State State { get; }
+        ITransactionListResponse Transactions { get; }
+        DateTime Updated { get; }
+        IUrls Urls { get; }
+        string UserAgent { get; }
+        Amount VatAmount { get; }
+    }
+}
---
+++ src/SwedbankPay.Sdk.Infrastructure/PaymentInstruments/PaymentInstrumentBase.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace SwedbankPay.Sdk.PaymentInstruments
+{
+    internal class PaymentInstrumentBase : IPaymentInstrumentBase
+    {
+        public PaymentInstrumentBase(IPaymentInstrumentBase payment)
+        {
+            Amount = payment.Amount;
+            VatAmount = payment.VatAmount;
+            RemainingCaptureAmount = payment.RemainingCaptureAmount;
+            RemainingCancellationAmount = payment.RemainingCancellationAmount;
+            RemainingReversalAmount = payment.RemainingReversalAmount;
+            Created = payment.Created;
+            Updated = payment.Updated;
+            Currency = payment.Currency;
+            Description = payment.Description;
+            Id = payment.Id;
+            Instrument = payment.Instrument;
+            Language = payment.Language;
+            Number = payment.Number;
+            Operation = payment.Operation;
+            PayeeInfo = payment.PayeeInfo;
+            PayerReference = payment.PayerReference;
+            InitiatingSystemUserAgent = payment.InitiatingSystemUserAgent;
+            Prices = payment.Prices;
+            State = payment.State;
+            Urls = payment.Urls;
+            UserAgent = payment.UserAgent;
+            Metadata = payment.Metadata;
+            Cancellations = payment.Cancellations;
+            Captures = payment.Captures;
+            Reversals = payment.Reversals;
+            Transactions = payment.Transactions;
+            Intent = payment.Intent;
+        }
+
+        public Amount Amount { get; }
+
+        public Amount RemainingCaptureAmount { get; }
+
+        public Amount RemainingCancellationAmount { get; }
+
+        public Amount RemainingReversalAmount { get; }
+
+        public ICancellationsListResponse Cancellations { get; }
+
+        public ICapturesListResponse Captures { get; }
+
+        public DateTime Created { get; }
+
+        public DateTime Updated { get; }
+
+        public Currency Currency { get; }
+
+        public string Description { get; }
+
+        public Uri Id { get; }
+
+        public PaymentInstrument Instrument { get; }
+
+        public PaymentIntent Intent { get; }
+
+        public Language Language { get; }
+
+        public long Number { get; }
+
+        public Operation Operation { get; }
+
+        public PayeeInfo PayeeInfo { get; }
+
+        public string PayerReference { get; }
+
+        public string InitiatingSystemUserAgent { get; }
+
+        public IPricesListResponse Prices { get; }
+
+        public IReversalsListResponse Reversals { get; }
+
+        public State State { get; }
+
+        public ITransactionListResponse Transactions { get; }
+
+        public IUrls Urls { get; }
+
+        public string UserAgent { get; }
+
+        public MetadataResponse Metadata { get; }
+
+        public Amount VatAmount { get; }
+    }
+}
