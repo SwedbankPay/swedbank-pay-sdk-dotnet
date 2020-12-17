69a11b766c5d9589aaa4595fbfcb5988a57798d1
Daniel Granerud <daniel.granerud@payex.com>
2020-12-17 10:34:26 +0100

    Revert "Remove lists from constructors in Trustly"
    This reverts commit d61ae765bce293e2a0ac90b4fa6793313bfb5038.

--- src/SwedbankPay.Sdk/PaymentInstruments/Trustly/TrustlyPaymentRequest.cs
+++ src/SwedbankPay.Sdk/PaymentInstruments/Trustly/TrustlyPaymentRequest.cs
@@ -11,6 +11,7 @@ namespace SwedbankPay.Sdk.PaymentInstruments.Trustly
         /// Instantiates a new <see cref="TrustlyPaymentRequest"/> with the provided parameters.
         /// </summary>
         /// <param name="currency">The wanted currency of the payment.</param>
+        /// <param name="prices">Prices object describing the payment price with different payment methods.</param>
         /// <param name="description">Textual descroption of the payment.</param>
         /// <param name="payerReference">A reference to the payer in the merchant system.</param>
         /// <param name="userAgent">The payers UserAgent string.</param>
@@ -19,6 +20,7 @@ namespace SwedbankPay.Sdk.PaymentInstruments.Trustly
         /// <param name="payeeInfo">Object with merchant information.</param>
         public TrustlyPaymentRequest(
             Currency currency,
+            List<IPrice> prices,
             string description,
             string payerReference,
             string userAgent,
@@ -26,7 +28,7 @@ namespace SwedbankPay.Sdk.PaymentInstruments.Trustly
             IUrls urls,
             PayeeInfo payeeInfo)
         {
-            Payment = new TrustlyPaymentDetails(currency, description, payerReference, userAgent, language,
+            Payment = new TrustlyPaymentDetails(currency, prices, description, payerReference, userAgent, language,
                                                urls, payeeInfo);
         }
 
