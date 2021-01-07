7ddc39f4508cba36c1cd0edc56e86be68a293d16
Daniel Granerud <daniel.granerud@payex.com>
2021-01-07 15:47:18 +0100

    Codacy fix

--- src/Samples/Sample.AspNetCore.SystemTests/Test/PaymentTests/PaymentOrder/Standard/StandardPaymentOrderReversalTests.cs
+++ src/Samples/Sample.AspNetCore.SystemTests/Test/PaymentTests/PaymentOrder/Standard/StandardPaymentOrderReversalTests.cs
@@ -66,7 +66,7 @@ namespace Sample.AspNetCore.SystemTests.Test.PaymentTests.PaymentOrder.Standard
 
             while (order.PaymentOrder.CurrentPayment.Payment.Transactions.TransactionList.First(x => x.Type == TransactionType.Reversal).State != State.Completed && counter <= 15)
             {
-                await Task.Delay(1000);
+                await Task.Delay(1000).ConfigureAwait(false);
                 order = await SwedbankPayClient.PaymentOrders.Get(orderLink, PaymentOrderExpand.All);
                 counter++;
             }
