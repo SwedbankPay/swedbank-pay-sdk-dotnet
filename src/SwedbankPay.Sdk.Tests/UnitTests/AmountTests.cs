b8a3c3c933517b2a82211c9b93ca7352f6a1f88e
NullableInt <daniel.granerud@payex.com>
2021-01-19 10:00:48 +0100

    Add unit test to check that we support large amounts

---
+++ src/SwedbankPay.Sdk.Tests/UnitTests/AmountTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SwedbankPay.Sdk.Tests.UnitTests
+{
+    public class AmountTests
+    {
+        [Fact]
+        public void Amount_SupportsLargeAmount_WithoutException()
+        {
+            _ = new Amount(9999999900);
+        }
+    }
+}
