b645afb6525ccad8ae91e1b2b2dc403a99c158f2
Daniel Granerud <daniel.granerud@payex.com>
2021-02-03 07:54:20 +0100

    Update version setting for NuGet package

--- .github/workflows/nuget_push.sh
+++ .github/workflows/nuget_push.sh
@@ -33,7 +33,7 @@ fi
 
 sanitized_version_number=${VERSION_NUMBER//\+/.}
 
-dotnet pack "$PROJECT_FILE" -p:NuspecFile=../SwedbankPay.Sdk.nuspec -p:NuspecBasePath=. -p:NuspecProperties="version=$sanitized_version_number" -o nugets/
+dotnet pack "$PROJECT_FILE" -p:NuspecFile=../SwedbankPay.Sdk.nuspec -p:NuspecBasePath=. -p:NuspecProperties="version=$sanitized_version_number" -p:PackageVersion="$sanitized_version_number" -o nugets/
 
 if [[ "${PUBLISH:-false}" = "true" ]]
 then
