2940c5e23f63ed8dbcb426818504467522bd173d
Daniel Sundqvist <daniel.sundqvist@authority.se>
2021-02-10 15:32:34 +0100

    Added OrderItems to dto object

--- src/SwedbankPay.Sdk.Infrastructure/PaymentOrders/PaymentOrderUpdateRequestDetailsDto.cs
+++ src/SwedbankPay.Sdk.Infrastructure/PaymentOrders/PaymentOrderUpdateRequestDetailsDto.cs
@@ -1,18 +1,23 @@
-﻿namespace SwedbankPay.Sdk.PaymentOrders
+﻿using System.Collections.Generic;
+
+namespace SwedbankPay.Sdk.PaymentOrders
 {
-    internal class PaymentOrderUpdateRequestDetailsDto
-    {
-        public PaymentOrderUpdateRequestDetailsDto(PaymentOrderUpdateRequestDetails paymentOrder)
-        {
-            Amount = paymentOrder.Amount.InLowestMonetaryUnit;
-            Operation = paymentOrder.Operation.Value;
-            VatAmount = paymentOrder.VatAmount?.InLowestMonetaryUnit;
-        }
+	internal class PaymentOrderUpdateRequestDetailsDto
+	{
+		public PaymentOrderUpdateRequestDetailsDto(PaymentOrderUpdateRequestDetails paymentOrder)
+		{
+			Amount = paymentOrder.Amount.InLowestMonetaryUnit;
+			Operation = paymentOrder.Operation.Value;
+			VatAmount = paymentOrder.VatAmount?.InLowestMonetaryUnit;
+			OrderItems = paymentOrder.OrderItems;
+		}
 
-        public long Amount { get; }
+		public long Amount { get; }
 
-        public string Operation { get; }
+		public string Operation { get; }
 
-        public long? VatAmount { get; }
-    }
+		public long? VatAmount { get; }
+
+		public IList<IOrderItem> OrderItems { get; set; }
+	}
 }
