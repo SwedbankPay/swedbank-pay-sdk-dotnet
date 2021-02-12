66e32419ca8aae6657fe7c39606a76a9412436b9
Daniel Granerud <daniel.granerud@payex.com>
2021-02-12 10:14:01 +0100

    Convert indentation to spaces

--- src/SwedbankPay.Sdk.Infrastructure/PaymentOrders/PaymentOrderUpdateRequestDetailsDto.cs
+++ src/SwedbankPay.Sdk.Infrastructure/PaymentOrders/PaymentOrderUpdateRequestDetailsDto.cs
@@ -2,25 +2,25 @@
 
 namespace SwedbankPay.Sdk.PaymentOrders
 {
-	internal class PaymentOrderUpdateRequestDetailsDto
-	{
-		public PaymentOrderUpdateRequestDetailsDto(PaymentOrderUpdateRequestDetails paymentOrder)
-		{
-			Amount = paymentOrder.Amount.InLowestMonetaryUnit;
-			Operation = paymentOrder.Operation.Value;
-			VatAmount = paymentOrder.VatAmount?.InLowestMonetaryUnit;
+    internal class PaymentOrderUpdateRequestDetailsDto
+    {
+        public PaymentOrderUpdateRequestDetailsDto(PaymentOrderUpdateRequestDetails paymentOrder)
+        {
+            Amount = paymentOrder.Amount.InLowestMonetaryUnit;
+            Operation = paymentOrder.Operation.Value;
+            VatAmount = paymentOrder.VatAmount?.InLowestMonetaryUnit;
             foreach (var item in paymentOrder.OrderItems)
             {
                 OrderItems.Add(new OrderItemDto(item));
             }
-		}
+        }
 
-		public long Amount { get; }
+        public long Amount { get; }
 
-		public string Operation { get; }
+        public string Operation { get; }
 
-		public long? VatAmount { get; }
+        public long? VatAmount { get; }
 
-		public IList<OrderItemDto> OrderItems { get; } = new List<OrderItemDto>();
-	}
+        public IList<OrderItemDto> OrderItems { get; } = new List<OrderItemDto>();
+    }
 }
