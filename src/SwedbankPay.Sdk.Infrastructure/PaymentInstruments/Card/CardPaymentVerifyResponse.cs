487d4f1ae5b25a6dffa8964db3f6275c44c4a711
Daniel Granerud <daniel.granerud@payex.com>
2021-02-23 13:36:45 +0100

    Add some models for verify request in card

---
+++ src/SwedbankPay.Sdk.Infrastructure/PaymentInstruments/Card/CardPaymentVerifyResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwedbankPay.Sdk.PaymentInstruments.Card
+{
+    internal class CardPaymentVerifyResponse : Identifiable, ICardPaymentVerifyResponse
+    {
+        public CardPaymentVerifyResponse(Uri id, IList<ICardPaymentAuthorization> verificationList)
+            : base(id)
+        {
+            VerificationList = verificationList ?? throw new ArgumentNullException(nameof(verificationList));
+        }
+
+        public IList<ICardPaymentAuthorization> VerificationList { get; }
+    }
+}
---
+++ src/SwedbankPay.Sdk/PaymentInstruments/Card/ICardPaymentVerifyResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace SwedbankPay.Sdk.PaymentInstruments.Card
+{
+    /// <summary>
+    /// Response of verifying a card payment.
+    /// </summary>
+    public interface ICardPaymentVerifyResponse : IIdentifiable
+    {
+        /// <summary>
+        /// Available list of verifications.
+        /// </summary>
+        IList<ICardPaymentAuthorization> VerificationList { get; }
+    }
+}
