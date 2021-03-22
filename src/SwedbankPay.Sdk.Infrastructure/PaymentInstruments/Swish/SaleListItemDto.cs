45bb3d18212d3fc649ed77d617cc11aac0032826
Daniel Sundqvist <daniel.sundqvist@authority.se>
2021-03-22 16:44:07 +0100

    Corrected name against API and added constructor for setting values

--- src/SwedbankPay.Sdk.Infrastructure/PaymentInstruments/Swish/SaleListItemDto.cs
+++ src/SwedbankPay.Sdk.Infrastructure/PaymentInstruments/Swish/SaleListItemDto.cs
@@ -2,14 +2,25 @@
 
 namespace SwedbankPay.Sdk.PaymentInstruments.Swish
 {
-    internal class SaleListItemDto
-    {
-        public DateTime Date { get; }
-        public Uri Id { get; }
-        public string PayerAlias { get; }
-        public string PaymentRequestToken { get; }
-        public string SwishPaymentReference { get; }
-        public string SwishStatus { get; }
-        public TransactionDto Transaction { get; }
-    }
+	internal class SaleListItemDto
+	{
+		public SaleListItemDto(DateTime date, Uri id, string payerAlias, string paymentRequestToken, string swishPaymentReference, string swishStatus, TransactionDto transaction)
+		{
+			Date = date;
+			Id = id;
+			PayerAlias = payerAlias;
+			PaymentRequestToken = paymentRequestToken;
+			SwishPaymentReference = swishPaymentReference;
+			SwishStatus = swishStatus;
+			Transaction = transaction;
+		}
+
+		public DateTime Date { get; }
+		public Uri Id { get; }
+		public string PayerAlias { get; }
+		public string PaymentRequestToken { get; }
+		public string SwishPaymentReference { get; }
+		public string SwishStatus { get; }
+		public TransactionDto Transaction { get; }
+	}
 }
