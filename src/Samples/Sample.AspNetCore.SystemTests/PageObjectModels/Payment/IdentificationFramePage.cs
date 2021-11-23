0896a4f426d5e64e22ac693dcc185bbdb87f6606
Clement Joye <clement.joye@authority.se>
2021-11-23 17:32:24 +0100

    Updated ui tests (fill in contact details for default data when needed)

--- src/Samples/Sample.AspNetCore.SystemTests/PageObjectModels/Payment/IdentificationFramePage.cs
+++ src/Samples/Sample.AspNetCore.SystemTests/PageObjectModels/Payment/IdentificationFramePage.cs
@@ -11,5 +11,15 @@ namespace Sample.AspNetCore.SystemTests.PageObjectModels.Payment
         [FindById("px-submit")] public Button<_> Next { get; private set; }
 
         [FindById("msisdn")] public TelInput<_> PhoneNumber { get; private set; }
+
+        [FindById("ssn")] public TextInput<_> PersonalNumber { get; private set; }
+        
+        [FindByClass("px-button custom-button")] public Button<_> SaveMyInformation { get; private set; }
+
+        [FindById("firstName")] public TextInput<_> FirstName { get; private set; }
+        [FindById("lastName")] public TextInput<_> LastName { get; private set; }
+        [FindById("streetAddress")] public TextInput<_> Address { get; private set; }
+        [FindById("zipCode")] public TextInput<_> ZipCode { get; private set; }
+        [FindById("city")] public TextInput<_> City { get; private set; }
     }
 }
