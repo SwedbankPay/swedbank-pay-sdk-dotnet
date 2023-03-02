5a32274a232003787bc695c95b723843ce35b9ba
Anton Sehovac <81076860+antonsehovac@users.noreply.github.com>
2023-03-02 11:35:45 +0100

    Update MobilePayPaymentAuthorizationDto.cs

--- src/SwedbankPay.Sdk.Infrastructure/PaymentInstruments/MobilePay/MobilePayPaymentAuthorizationDto.cs
+++ src/SwedbankPay.Sdk.Infrastructure/PaymentInstruments/MobilePay/MobilePayPaymentAuthorizationDto.cs
@@ -4,16 +4,16 @@ namespace SwedbankPay.Sdk.PaymentInstruments.MobilePay
 {
     internal class MobilePayPaymentAuthorizationDto
     {
-        public string AcquirerStan { get; }
-        public string AcquirerTerminalId { get; }
-        public DateTime AcquirerTransactionTime { get; }
-        public string AcquirerTransactionType { get; }
-        public string CardBrand { get; }
-        public string CardType { get; }
-        public string CountryCode { get; }
-        public string ExpiryDate { get; }
-        public string MaskedPan { get; }
-        public string PanToken { get; }
-        public AuthorizationTransactionDto Transaction { get; }
+        public string AcquirerStan { get; set; }
+        public string AcquirerTerminalId { get; set; }
+        public DateTime AcquirerTransactionTime { get; set; }
+        public string AcquirerTransactionType { get; set; }
+        public string CardBrand { get; set; }
+        public string CardType { get; set; }
+        public string CountryCode { get; set; }
+        public string ExpiryDate { get; set; }
+        public string MaskedPan { get; set; }
+        public string PanToken { get; set; }
+        public AuthorizationTransactionDto Transaction { get; set; }
     }
 }
