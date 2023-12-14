cf99fdf0b81bc8707907d1ce9673b69fa93ce5df
Daniel Sundqvist <daniel.sundqvist@authority.se>
2023-12-14 20:23:41 +0100

    Enhance security and validation in SwedbankPayClient
    Added a method to ensure Tls12 security protocol. Separated the validation of HttpClient parameters into a distinct function. This enhances code readability, maintains consistent error handling and ensures the security protocol settings.

--- src/SwedbankPay.Sdk/SwedbankPayClient.cs
+++ src/SwedbankPay.Sdk/SwedbankPayClient.cs
@@ -7,40 +7,43 @@ public class SwedbankPayClient : ISwedbankPayClient
 {
     public SwedbankPayClient(HttpClient httpClient, IPaymentOrdersResource paymentOrders)
     {
-        if (!ServicePointManager.SecurityProtocol.HasFlag(SecurityProtocolType.Tls12))
-        {
-            ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12;
-        }
-
-        if (httpClient == null)
-        {
-            throw new ArgumentNullException(nameof(httpClient));
-        }
-
-        if (httpClient.BaseAddress == null)
-        {
-            throw new ArgumentNullException(nameof(httpClient), $"{nameof(httpClient.BaseAddress)} cannot be null.");
-        }
-
-        if (httpClient.DefaultRequestHeaders?.Authorization?.Parameter == null)
-        {
-            throw new ArgumentException($"Please configure the {nameof(httpClient)} with an Authorization header.");
-        }
-
-        if (!httpClient.DefaultRequestHeaders.Contains("User-Agent"))
-        {
-            httpClient.DefaultRequestHeaders.Add("User-Agent", UserAgent.Default);
-        }
-
+        EnsureTls12SecurityProtocol();
+        ValidateHttpClient(httpClient);
         PaymentOrders = paymentOrders ?? throw new ArgumentNullException(nameof(paymentOrders));
     }
 
     public SwedbankPayClient(HttpClient httpClient) :
-        this(
-            httpClient,
-            new PaymentOrdersResource(httpClient))
+        this(httpClient, new PaymentOrdersResource(httpClient))
     {
     }
 
     public IPaymentOrdersResource PaymentOrders { get; }
+
+    private void EnsureTls12SecurityProtocol()
+    {
+        if (!ServicePointManager.SecurityProtocol.HasFlag(SecurityProtocolType.Tls12))
+        {
+            ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12;
+        }
+    }
+
+    private void ValidateHttpClient(HttpClient httpClient)
+    {
+        if (httpClient == null)
+        {
+            throw new ArgumentNullException(nameof(httpClient));
+        }
+        if (httpClient.BaseAddress == null)
+        {
+            throw new ArgumentNullException(nameof(httpClient), $"{nameof(httpClient.BaseAddress)} cannot be null.");
+        }
+        if (httpClient.DefaultRequestHeaders.Authorization?.Parameter == null)
+        {
+            throw new ArgumentException($"Please configure the {nameof(httpClient)} with an Authorization header.");
+        }
+        if (!httpClient.DefaultRequestHeaders.Contains("User-Agent"))
+        {
+            httpClient.DefaultRequestHeaders.Add("User-Agent", UserAgent.Default);
+        }
+    }
 }
