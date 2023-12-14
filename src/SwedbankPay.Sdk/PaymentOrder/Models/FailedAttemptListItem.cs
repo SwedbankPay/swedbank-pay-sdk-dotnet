b3610a55d823d451db6c38e41edb0cbc251752bc
Daniel Sundqvist <daniel.sundqvist@authority.se>
2023-12-14 16:47:39 +0100

    Added FailedStatus

--- src/SwedbankPay.Sdk/PaymentOrder/Models/FailedAttemptListItem.cs
+++ src/SwedbankPay.Sdk/PaymentOrder/Models/FailedAttemptListItem.cs
@@ -5,7 +5,7 @@ public record FailedAttemptListItem
     public DateTime Created { get; set; }
     public string? Instrument { get; set; }
     public long Number { get; set; }
-    public string? Status { get; set; }
+    public FailedStatus? Status { get; set; }
     public Problem? Problem { get; set; }
 
     internal FailedAttemptListItem(FailedAttemptListItemDto dto)
--- src/SwedbankPay.Sdk/PaymentOrder/Models/FailedAttemptListItemDto.cs
+++ src/SwedbankPay.Sdk/PaymentOrder/Models/FailedAttemptListItemDto.cs
@@ -5,7 +5,7 @@ internal record FailedAttemptListItemDto
     public DateTime Created { get; init; }
     public string? Instrument { get; init; }
     public long Number { get; init; }
-    public string? Status { get; init; }
+    public string Status { get; init; } = null!;
     public ProblemDto? Problem { get; init; }
 
     public FailedAttemptListItem Map()
