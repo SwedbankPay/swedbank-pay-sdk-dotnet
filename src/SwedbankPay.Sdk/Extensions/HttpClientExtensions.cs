a1e13a004f60c06f9eb83f00d1ee1780c1961dcf
Daniel Sundqvist <daniel.sundqvist@authority.se>
2023-12-15 14:48:31 +0100

    Refactor HttpClientExtensions to improve code readability
    This commit simplifies the HttpClientExtensions class by refactoring the request and response processing methods. It breaks down the responsibility of handling responses into smaller parts, separating success and error states into different methods. This makes the code more readable and easier to maintain. Removed try-catch blocks in favour of explicit error handling methods. Further, the JsonSerializer settings are now held as a private static readonly field for reuse to avoid repetition.

--- src/SwedbankPay.Sdk/Extensions/HttpClientExtensions.cs
+++ src/SwedbankPay.Sdk/Extensions/HttpClientExtensions.cs
@@ -8,105 +8,12 @@ namespace SwedbankPay.Sdk.Extensions;
 
 public static class HttpClientExtensions
 {
+    private static readonly JsonSerializerOptions? SerializationSettings = JsonSerialization.JsonSerialization.Settings;
+
     public static async Task<T?> GetAsJsonAsync<T>(this HttpClient httpClient, Uri uri)
     {
-        var apiResponse = await httpClient.GetAsync(uri);
-        var responseString = await apiResponse.Content.ReadAsStringAsync();
-
-        if (!apiResponse.IsSuccessStatusCode)
-        {
-            IProblem? problemResponseDto = null;
-            if (!string.IsNullOrEmpty(responseString))
-            {
-                problemResponseDto = JsonSerializer.Deserialize<ProblemDto>(responseString)?.Map();
-            }
-
-            var errorMessage = BuildErrorMessage(responseString, uri, apiResponse);
-            throw new HttpResponseException(
-                apiResponse,
-                problemResponseDto,
-                errorMessage);
-        }
-
-        return JsonSerializer.Deserialize<T>(responseString, JsonSerialization.JsonSerialization.Settings);
-    }
-
-    private static async Task<T?> SendAndProcessAsync<T>(this HttpClient httpClient, HttpMethod httpMethod, Uri uri,
-        object? payload)
-        where T : class
-    {
-        using var httpRequestMessage = new HttpRequestMessage(httpMethod, uri);
-        if (payload != null)
-        {
-            var content = JsonSerializer.Serialize(payload, JsonSerialization.JsonSerialization.Settings);
-            try
-            {
-                httpRequestMessage.Content = new StringContent(content, Encoding.UTF8);
-                var header = new MediaTypeHeaderValue("application/json");
-                header.Parameters.Add(new NameValueHeaderValue("version", "3.1"));
-                httpRequestMessage.Content.Headers.ContentType = header;
-            }
-            catch (Exception e)
-            {
-                var a = e.Message;
-            }
-        }
-
-        using var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
-
-        var httpResponseContent = string.Empty;
-        try
-        {
-            httpResponseContent = await httpResponseMessage.Content.ReadAsStringAsync();
-            if (!httpResponseMessage.IsSuccessStatusCode)
-            {
-                string? errorMessage;
-                if (string.IsNullOrEmpty(httpResponseContent))
-                {
-                    var httpStatusCode = (int)httpResponseMessage.StatusCode;
-                    var problem = new Problem(httpResponseContent,
-                        httpStatusCode,
-                        httpResponseContent,
-                        httpResponseContent);
-                    errorMessage = BuildErrorMessage(httpResponseContent);
-                    throw new HttpResponseException(httpResponseMessage, problem, errorMessage);
-                }
-
-                var problemResponseDto = JsonSerializer.Deserialize<ProblemDto>(httpResponseContent,
-                    JsonSerialization.JsonSerialization.Settings);
-                var problemResponse = problemResponseDto?.Map();
-                errorMessage = BuildErrorMessage(httpResponseContent);
-
-                throw new HttpResponseException(
-                    httpResponseMessage,
-                    problemResponse,
-                    errorMessage);
-            }
-
-            return JsonSerializer.Deserialize<T>(httpResponseContent, JsonSerialization.JsonSerialization.Settings);
-        }
-        catch (HttpResponseException ex)
-        {
-            ex.Data.Add(nameof(httpResponseContent), httpResponseContent);
-            throw ex;
-        }
-
-        catch (Exception e)
-        {
-            e.Data.Add(nameof(httpResponseContent), httpResponseContent);
-            throw e;
-        }
-        string BuildErrorMessage(string? httpResponseBody)
-        {
-            return
-                $"{httpRequestMessage.Method}: {httpRequestMessage.RequestUri} failed with error code {httpResponseMessage.StatusCode} using bearer token {httpClient.DefaultRequestHeaders?.Authorization?.Parameter}. Response body: {httpResponseBody}";
-        }
-    }
-
-    private static string BuildErrorMessage(string httpResponseBody, Uri uri, HttpResponseMessage httpResponse)
-    {
-        return
-            $"GET: {uri} failed with error code {httpResponse.StatusCode}. Response body: {httpResponseBody}";
+        using var apiResponse = await httpClient.GetAsync(uri);
+        return await ProcessResponse<T>(apiResponse, httpClient, uri);
     }
 
     public static Task<T?> PostAsJsonAsync<T>(this HttpClient httpClient, Uri uri, object? payload)
@@ -120,5 +27,74 @@ public static class HttpClientExtensions
         where T : class
     {
         return httpClient.SendAndProcessAsync<T>(httpMethod, uri, payload);
+    }
+
+    private static async Task<T?> SendAndProcessAsync<T>(this HttpClient httpClient, HttpMethod httpMethod, Uri uri,
+        object? payload = null)
+        where T : class
+    {
+        using var httpRequestMessage = new HttpRequestMessage(httpMethod, uri);
+        if (payload != null)
+        {
+            var content = JsonSerializer.Serialize(payload, SerializationSettings);
+            httpRequestMessage.Content = new StringContent(content, Encoding.UTF8);
+            var header = new MediaTypeHeaderValue("application/json");
+            header.Parameters.Add(new NameValueHeaderValue("version", "3.1"));
+            httpRequestMessage.Content.Headers.ContentType = header;
+        }
+
+        using var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+        return await ProcessResponse<T>(httpResponseMessage, httpClient, uri);
+    }
+
+    private static async Task<T?> ProcessResponse<T>(HttpResponseMessage httpResponseMessage, HttpClient httpClient, Uri uri)
+    {
+        var httpResponseContent = await httpResponseMessage.Content.ReadAsStringAsync();
+        if (!httpResponseMessage.IsSuccessStatusCode)
+        {
+            HandleUnsuccessfulResponse(httpResponseContent, httpResponseMessage, httpClient, uri);
+        }
+
+        return JsonSerializer.Deserialize<T>(httpResponseContent, SerializationSettings);
+    }
+
+    private static void HandleUnsuccessfulResponse(string httpResponseContent, HttpResponseMessage httpResponseMessage, HttpClient httpClient, Uri uri)
+    {
+        IProblem? problemResponseDto = null;
+        string? errorMessage;
+        if (string.IsNullOrEmpty(httpResponseContent))
+        {
+            var httpStatusCode = (int)httpResponseMessage.StatusCode;
+            var problem = new Problem(httpResponseContent,
+                httpStatusCode,
+                httpResponseContent,
+                httpResponseContent);
+            errorMessage = BuildErrorMessage(httpResponseContent, httpClient, httpResponseMessage);
+            throw new HttpResponseException(httpResponseMessage, problem, errorMessage);
+        }
+        
+        if (!string.IsNullOrEmpty(httpResponseContent))
+        {
+            problemResponseDto = JsonSerializer.Deserialize<ProblemDto>(httpResponseContent, SerializationSettings)?.Map();
+        }
+
+        errorMessage = BuildErrorMessage(httpResponseContent, uri, httpResponseMessage);
+        var httpResponseException = new HttpResponseException(
+            httpResponseMessage,
+            problemResponseDto,
+            errorMessage);
+        httpResponseException.Data.Add(nameof(httpResponseContent), httpResponseContent);
+        throw httpResponseException;
+    }
+
+    private static string BuildErrorMessage(string httpResponseBody, HttpClient httpClient,  HttpResponseMessage httpResponseMessage)
+    {
+        return
+            $"{httpResponseMessage.RequestMessage?.Method}: {httpResponseMessage.RequestMessage?.RequestUri} failed with error code {httpResponseMessage.StatusCode} using bearer token {httpClient.DefaultRequestHeaders?.Authorization?.Parameter}. Response body: {httpResponseBody}";
+    }
+    
+    private static string BuildErrorMessage(string httpResponseBody, Uri uri, HttpResponseMessage httpResponse)
+    {
+        return $"GET: {uri} failed with error code {httpResponse.StatusCode}. Response body: {httpResponseBody}";
     }
 }
