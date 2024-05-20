67f72c7a2f54e5484d71259c9f182686a707fdba
lounge <fredlunden@gmail.com>
2024-05-20 10:49:30 +0200

    Refactor PayeeInfo class and add SiteId property
    This update refactors the PayeeInfo class, removing unnecessary spaces, and improving the explanation of the Subsite property. A new property, SiteId has been added for handling split settlement transactions. The SiteId is used to specify the sub-merchant for AMEX transactions.

--- src/SwedbankPay.Sdk/PayeeInfo.cs
+++ src/SwedbankPay.Sdk/PayeeInfo.cs
@@ -5,22 +5,21 @@ namespace SwedbankPay.Sdk;
 /// </summary>
 public record PayeeInfo(string PayeeReference)
 {
-
     /// <summary>
     ///     The name of the payee, usually the name of the merchant.
     /// </summary>
     public string? PayeeName { get; init; }
-    
+
     /// <summary>
     ///     The order reference should reflect the order reference found in the merchant's systems.
     /// </summary>
     public string? OrderReference { get; init; }
-    
+
     /// <summary>
     ///     This is the unique id that identifies this payee (like merchant) set by PayEx.
     /// </summary>
     public string? PayeeId { get; init; }
-    
+
     /// <summary>
     ///     A unique reference, max 30 characters, set by the merchant system - this must be unique for each operation!
     ///     NOTE://PayEx may send either the transaction number OR the payeeReference as a reference to the acquirer.
@@ -31,13 +30,18 @@ public record PayeeInfo(string PayeeReference)
     ///     characters must be digits/numbers.
     /// </summary>
     public string PayeeReference { get; } = PayeeReference;
-    
+
     /// <summary>
-    ///     The subsite field can be used to perform split settlement on the payment. The subsites must be resolved with PayEx
-    ///     reconciliation before being used.
+    ///    The subsite field can be used to perform split settlement on the payment.
+    ///    The different subsite values must be resolved with Swedbank Pay reconciliation before being used.
+    ///    If you send in an unknown subsite value, it will be ignored and the payment will be settled using the merchant’s default settlement account.
+    ///    Must be in the format of A-Za-z0-9.
     /// </summary>
     public string? Subsite { get; init; }
+
+    /// <summary>
+    /// siteId is used for split settlement transactions when you, as a merchant, need to specify towards AMEX which sub-merchant the transaction belongs to.
+    /// Must be in the format of A-Za-z0-9.
+    /// </summary>
     public string? SiteId { get; init; }
 }
-
-
